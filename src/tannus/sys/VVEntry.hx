e438497bac293a9b92114b22ab8a5aceb1f16657
Ryan Davis <epikit@gmail.com>
2015-04-07 00:40:40 -0500

    Wrote the VirtualVolume class and its associated systems

---
+++ src/tannus/sys/VVEntry.hx
@@ -0,0 +1,213 @@
+package tannus.sys;
+
+import tannus.sys.Path;
+import tannus.sys.VVType;
+import tannus.sys.FileStat;
+import tannus.io.Byte;
+import tannus.io.ByteArray;
+
+/**
+  * Class to represent an entry in a VirtualVolume
+  */
+class VVEntry {
+	/* Constructor Function */
+	public function new(vv:VirtualVolume, nam:String, typ:VVType):Void {
+		name = nam;
+		content = null;
+		type = typ;
+		meta = new Map();
+		volume = vv;
+
+		__init();
+	}
+
+/* == Instance Methods === */
+
+	/**
+	  * Perform some dope-ass initialization stuff
+	  */
+	private function __init():Void {
+		cdate = Date.now();
+	}
+
+	/**
+	  * Marks [this] Entry as having been modified just now
+	  */
+	private inline function update():Void {
+		mdate = Date.now();
+	}
+
+	/**
+	  * List all files which are inside [this] Directory
+	  */
+	public function list():Array<VVEntry> {
+		if (isDirectory) {
+			var entries = volume.all();
+			return entries.filter(function(e) {
+				return (e.path.directory == path);
+			});
+		} else {
+			error('"$path" is a File!');
+		}
+	}
+
+	/**
+	  * Write to [this] File
+	  */
+	public function write(data : ByteArray):Void {
+		if (isFile || !(volume.exists(path))) {
+			content = data;
+			update();
+		} else {
+			error('"$path" is a Directory!');
+		}
+	}
+
+	/**
+	  * Reads from [this] File
+	  */
+	public function read():ByteArray {
+		if (isFile) {
+			if (content == null) {
+				return '';
+			} else {
+				return content;
+			}
+		} else {
+			error('"$path" cannot be read!');
+		}
+	}
+
+	/**
+	  * Appends [data] to [this] File
+	  */
+	public function append(data : ByteArray):Void {
+		if (isFile) {
+			content = read();
+			content += data;
+			update();
+		} else {
+			error('"$path" cannot be written to!');
+		}
+	}
+
+	/**
+	  * Renames [this] Entry
+	  */
+	public function rename(newname : String):Void {
+		//- [this] Entry is a File
+		if (isFile) {
+			name = newname;
+		}
+		
+		//- [this] Entry is a Directory
+		else {
+			var subs = list();
+
+			for (e in subs) {
+				var np:String = e.path.normalize();
+				np = StringTools.replace(np, path.normalize(), (new Path(newname).normalize()));
+				e.name = np;
+			}
+
+			name = newname;
+		}
+	}
+
+/* === Computed Instance Fields === */
+
+	/**
+	  * 'name' as a Path instance
+	  */
+	public var path(get, set):Path;
+	private inline function get_path():Path {
+		return name;
+	}
+	private inline function set_path(np : Path):Path {
+		name = np;
+		return name;
+	}
+
+	/**
+	  * The FileStat for [this] Entry
+	  */
+	public var stats(get, never):FileStat;
+	private function get_stats():FileStat {
+		if (isFile) {
+			return {
+				'size': (read().length),
+				'ctime': cdate,
+				'mtime': mdate
+			};
+		} 
+		else {
+			error('"$path" is a Directory!');
+		}
+	}
+
+	/**
+	  * Whether [this] Entry is a file
+	  */
+	public var isFile(get, never):Bool;
+	private function get_isFile():Bool {
+		switch (type) {
+			case VVFile: return true;
+			default: return false;
+		}
+	}
+
+	/**
+	  * Whether [this] Entry is a directory
+	  */
+	public var isDirectory(get, never):Bool;
+	private inline function get_isDirectory():Bool {
+		return !isFile;
+	}
+
+	/**
+	  * The data-of-creation of [this] Entry
+	  */
+	public var cdate(get, set):Date;
+	private function get_cdate():Date {
+		return meta['cdate'];
+	}
+	private function set_cdate(cd : Date):Date {
+		var _cd:Null<Date> = meta['cdate'];
+		if (_cd != null && Std.is(_cd, Date)) {
+			return _cd;
+		}
+		else {
+			return (meta['cdate'] = cd);
+		}
+	}
+
+	/**
+	  * The last-modified date
+	  */
+	public var mdate(get, set):Date;
+	private function get_mdate():Date {
+		var m:Null<Date> = meta['mdate'];
+		
+		return (m != null ? m : cdate);
+	}
+	private function set_mdate(nm : Date):Date {
+		return (meta['mdate'] = nm);
+	}
+
+/* === Instance Fields === */
+
+	public var name:String;
+	public var content:Null<ByteArray>;
+	public var type:VVType;
+	private var meta:Map<String, Dynamic>;
+	public var volume:VirtualVolume;
+
+/* === Class Methods === */
+
+	/**
+	  * Throws an error with the given message
+	  */
+	private static inline function error(msg : String):Void {
+		throw 'IOError: $msg';
+	}
+}
---
+++ src/tannus/sys/VirtualVolume.hx
@@ -0,0 +1,252 @@
+package tannus.sys;
+
+import tannus.io.Byte;
+import tannus.io.ByteArray;
+import tannus.io.Ptr;
+import tannus.sys.Path;
+import tannus.sys.VVEntry;
+import tannus.sys.VVType;
+import tannus.sys.FileStat;
+
+/**
+  * Class which acts as a virtual filesystem
+  */
+class VirtualVolume {
+	/* Constructor Function */
+	public function new(nam : String):Void {
+		name = nam;
+		entries = new Array();
+	}
+
+/* === Instance Methods == */
+
+	/**
+	  * Get the Array of all entries in [this] VirtualVolume
+	  */
+	public inline function all():Array<VVEntry> {
+		return entries;
+	}
+
+	/**
+	  * Retrieves an entry by name
+	  */
+	public function getEntry(name : String):Null<VVEntry> {
+		name = normal(name);
+		for (f in entries) {
+			if (f.path == name) {
+				return f;
+			}
+		}
+		return null;
+	}
+
+	/**
+	  * Creates a new Entry and adds it to [this] Volume
+	  */
+	private inline function create(name:String, type:VVType):VVEntry {
+		name = normal(name);
+		var e = new VVEntry(this, name, type);
+		entries.push( e );
+		return e;
+	}
+
+	/**
+	  * Takes a Path, and validates that it's valid
+	  */
+	private function validatePath(p : Path):Void {
+		name = normal(name);
+		var _p:Path = p;
+		while (true) {
+			if (_p.root) {
+				break;
+			}
+			else {
+				_p = (_p.directory);
+				
+				if (!exists(_p)) {
+					error('No such file or directory "$_p"!');
+				}
+			}
+		}
+	}
+
+	/**
+	  * Checks whether there is any entry named [name]
+	  */
+	public function exists(name : String):Bool {
+		name = normal(name);
+		var p:Path = name;
+		return (getEntry(p) != null);
+	}
+
+	/**
+	  * Checks whether an entry is a Directory
+	  */
+	public function isDirectory(name : String):Bool {
+		name = normal(name);
+		var e = getEntry(name);
+		if (e == null) {
+			return false;
+		} else {
+			switch (e.type) {
+				case VVFolder:
+					return true;
+
+				default:
+					return false;
+			}
+		}
+	}
+
+	/**
+	  * Creates a new Directory
+	  */
+	public function createDirectory(name : String):Void {
+		name = normal(name);
+		validatePath( name );
+		create(name, VVFolder);
+	}
+
+	/**
+	  * Deletes a Directory
+	  */
+	public function deleteDirectory(name : String):Void {
+		name = normal(name);
+		if (isDirectory(name)) {
+			var e = getEntry(name);
+			var subs = e.list();
+			if (subs.length == 0) {
+				entries.remove( e );
+			} else {
+				error('Directory not empty "$name"!');
+			}
+		}
+	}
+
+	/**
+	  * Retrieve an Array of all children of the given directory
+	  */
+	public function readDirectory(name : String):Array<String> {
+		name = normal(name);
+		if (isDirectory(name)) {
+			var e = getEntry(name);
+			return e.list().map(function(e) return e.name);
+		} else {
+			error('"$name" is not a Directory!');
+		}
+	}
+
+	/**
+	  * Writes to a File
+	  */
+	public function write(path:String, data:ByteArray):Void {
+		path = normal(path);
+		validatePath(path);
+		
+		var e = getEntry(path);
+		if (e == null) {
+			e = create(path, VVFile);
+		}
+
+		e.write( data );
+	}
+
+	/**
+	  * Reads from a file
+	  */
+	public function read(path : String):ByteArray {
+		path = normal(path);
+		var e = getEntry(path);
+
+		if (e != null && e.isFile) {
+			return e.read();
+		} else {
+			error('"$path" is either a Directory, or does not exist!');
+		}
+	}
+
+	/**
+	  * Append to a File
+	  */
+	public function append(path:String, data:ByteArray):Void {
+		path = normal(path);
+		var e = getEntry(path);
+
+		if (e != null && e.isFile) {
+			e.append( data );
+		}
+		else {
+			error('"$path" cannot be written to!');
+		}
+	}
+
+	/**
+	  * Deletes a given File
+	  */
+	public function deleteFile(path : String):Void {
+		path = normal(path);
+		var e = getEntry(path);
+
+		if (e != null && e.isFile) {
+			entries.remove( e );
+		}
+		else {
+			error('Cannot delete "$path"!');
+		}
+	}
+
+	/**
+	  * Renames an entry
+	  */
+	public function rename(oldp:String, newp:String):Void {
+		oldp = normal(oldp);
+		newp = normal(newp);
+
+		if (exists(oldp)) {
+			validatePath( newp );
+
+			var e = getEntry(oldp);
+			e.rename(newp);
+		} 
+		else {
+			error('No such file or directory "$oldp"!');
+		}
+	}
+
+	/**
+	  * Gets the stats on a File
+	  */
+	public function stat(path : String):FileStat {
+		path = normal(path);
+		var e = getEntry(path);
+
+		if (e != null) {
+			return e.stats;
+		}
+		else {
+			error('No such file or directory "$path"!');
+		}
+	}
+
+
+/* === Instance Fields === */
+
+	public var name:String;
+	private var entries:Array<VVEntry>;
+
+/* === Class Methods === */
+
+	/**
+	  * Throws an error with the given message
+	  */
+	private static inline function error(msg : String):Void {
+		throw 'IOError: $msg';
+	}
+
+	/**
+	  * Does stuff
+	  */
+	private static inline function normal(name : String):String {
+		return (new Path(name).normalize());
+	}
+}
