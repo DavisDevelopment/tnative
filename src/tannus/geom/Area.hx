1992ae85c999c75c45232fc595e60b8fe9de1c60
Ryan Davis <epikit@gmail.com>
2015-04-08 19:59:55 -0500

    Reimplemented tannus.geom.Area as an abstract, which will unify with java.awt.Dimension and tannus.geom.Rectangle

--- src/tannus/geom/Area.hx
+++ src/tannus/geom/Area.hx
@@ -1,6 +1,54 @@
 package tannus.geom;
 
-class Area {
+import tannus.ds.TwoTuple;
+import tannus.geom.Rectangle;
+
+abstract Area (TwoTuple<Float, Float>) {
+	public inline function new(w:Float=0, h:Float=0):Void {
+		this = new TwoTuple(w, h);
+	}
+
+	/* 'width' field */
+	public var width(get, set):Float;
+	private inline function get_width() return this.one;
+	private inline function set_width(nw:Float) return (this.one = nw);
+
+	/* 'height' field */
+	public var height(get, set):Float;
+	private inline function get_height() return (this.two);
+	private inline function set_height(nh:Float) return (this.two = nh);
+
+/* === Type Casting === */
+
+	/* To Rectangle */
+	@:to
+	public function toRectangle():tannus.geom.Rectangle {
+		return new Rectangle(0, 0, width, height);
+	}
+
+	/* To String */
+	@:to
+	public function toString():String {
+		return 'Area(width=$width, height=$height)';
+	}
+
+	#if java
+	/* To java.awt.Dimension */
+	@:to
+	public function toJavaDimension():java.awt.Dimension {
+		return new java.awt.Dimension(i(width), i(height));
+	}
+
+	@:from
+	public static function fromJavaDimension(d : java.awt.Dimension):Area {
+		return new Area(d.width, d.height);
+	}
+	#end
+
+	private static inline function i(f:Float):Int return Math.round(f);
+}
+
+class OldArea {
 	/* Constructor Function */
 	public function new(w:Float=0, h:Float=0):Void {
 		width = w;
