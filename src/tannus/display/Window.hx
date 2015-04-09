2777aa1cc44b7ab5a3659a270c7985868e469ee0
Ryan Davis <epikit@gmail.com>
2015-04-08 23:49:42 -0500

    Created tannus.display.Window (abstract/extern/typedef thingy?) which forwards the TWindow interface from whatever implementation is currently being used

---
+++ src/tannus/display/Window.hx
@@ -0,0 +1,42 @@
+package tannus.display;
+
+import tannus.display.TWindow;
+
+import tannus.geom.Area;
+
+@:forward
+abstract Window (TWindow) {
+	public inline function new():Void {
+		#if (js && !node)
+
+			this = new tannus.display.backend.js.Window();
+
+		#elseif (flash || as3)
+
+			this = new tannus.display.backend.flash.Window();
+
+		#elseif java
+
+			this = new tannus.display.backend.java.Window();
+
+		#else
+			throw 'Cannot create Window on the current target';
+		#end
+	}
+
+/* === Alias 'nc_' (Non-Conflicting) Fields to Their Intended Names === */
+
+	/**
+	  * Window Title
+	  */
+	public var title(get, set):String;
+	private inline function get_title() return (this.nc_title);
+	private inline function set_title(nt:String) return (this.nc_title = nt);
+
+	/**
+	  * Window Size
+	  */
+	public var size(get, set):Area;
+	private inline function get_size() return (this.nc_size);
+	private inline function set_size(ns:Area) return (this.nc_size = ns);
+}
