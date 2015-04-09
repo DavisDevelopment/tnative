b880cc6d3f88bf0b52144b5747f65359d201ba83
Ryan Davis <epikit@gmail.com>
2015-04-08 20:41:01 -0500

    The HTML5 implementation of Window

---
+++ src/tannus/display/backend/js/Window.hx
@@ -0,0 +1,115 @@
+package tannus.display.backend.js;
+
+import tannus.display.backend.js.*;
+import tannus.display.TWindow;
+import tannus.geom.Area;
+import tannus.io.Signal;
+
+import js.html.Document;
+import js.html.CanvasElement;
+
+class Window implements TWindow {
+	/* Constructor Function */
+	public function new():Void {
+		instance = this;
+
+		//- Create Event Signals
+		resizeEvent = new Signal();
+		frameEvent = new Signal();
+		
+		//- Create a new Canvas
+		canvas = cast doc.createElement( 'canvas' );
+
+		__init();
+	}
+
+/* === Instance Methods === */
+
+	/**
+	  * Initialize [this] Window
+	  */
+	private inline function __init():Void {
+		
+		//- add my canvas to the window
+		doc.body.appendChild( canvas );
+		
+		//- Make [canvas] occupy entire browser window
+		makeFullScreen( canvas );
+
+		__bindEvents();
+		__frameLoop();
+	}
+
+	/**
+	  * Bind all useful Events into the Tannus System
+	  */
+	private function __bindEvents():Void {
+		var ebinder = new EventBinder( this );
+		ebinder.bind();
+	}
+
+	/**
+	  * Initiate the Frame-Loop
+	  */
+	private function __frameLoop():Void {
+		/**
+		  * We'll use [setInterval] for now, 
+		  * but we'll need to use requestAnimationFrame later
+		  */
+		win.setInterval(function() {
+			
+			frameEvent.broadcast(null);
+		}, 30);
+
+	}
+
+	/**
+	  * Make the canvas take up the full window
+	  */
+	//@:allow(tannus.display.backend.js.EventBinder)
+	public function makeFullScreen(can : CanvasElement):Void {
+		can.width = win.innerWidth;
+		can.height = win.innerHeight;
+	}
+
+
+/* === Computed Instance Fields === */
+
+	private var d(get, never):Document;
+	private inline function get_d() return js.Browser.document;
+	
+	private var doc(get, never):Dynamic;
+	private inline function get_doc() return js.Browser.document;
+
+	private var win(get, never):js.html.Window;
+	private inline function get_win() return js.Browser.window;
+
+	/* Window Title */
+	public var nc_title(get, set):String;
+	private inline function get_nc_title() return d.title;
+	private inline function set_nc_title(nt:String) return (d.title = nt);
+
+	/* Window Size */
+	public var nc_size(get, set):Area;
+	private inline function get_nc_size() return new Area(win.innerWidth, win.innerHeight);
+	private inline function set_nc_size(ns : Area):Area {
+		return nc_size;
+	}
+
+
+/* === Fields === */
+
+	//- Reference to THE instance of Window
+	public static var instance:Null<Window> = null;
+
+	//- The Canvas in use by [this] Window
+	public var canvas : CanvasElement;
+
+	//- Event fired when the Window is resized
+	public var resizeEvent : Signal<{then:Area, now:Area}>;
+
+	//- Event fired before the rendering of each frame
+	public var frameEvent : Signal<Dynamic>;
+}
+
+private typedef Win = js.html.Window;
