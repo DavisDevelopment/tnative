0b20fb39830c22cef4ec672dda6104edb1e89a9f
Ryan Davis <epikit@gmail.com>
2015-04-15 10:15:15 -0500

    Created PathStyle class, for easy management of all style-properties related to GraphicsPaths

---
+++ src/tannus/graphics/PathStyle.hx
@@ -0,0 +1,36 @@
+package tannus.graphics;
+
+import tannus.graphics.*;
+
+class PathStyle {
+	/* Constructor Function */
+	public function new():Void {
+		lineStyle = new LineStyle();
+
+		reset();
+	}
+
+/* === Instance Methods === */
+
+	/**
+	  * Restore [this] to it's default state
+	  */
+	public inline function reset():Void {
+		lineStyle.reset();
+	}
+
+	/**
+	  * Create and return a clone of [this]
+	  */
+	public inline function clone():PathStyle {
+		var c = new PathStyle();
+		c.lineStyle = lineStyle.clone();
+
+		return c;
+	}
+
+/* === Instance Fields === */
+
+	//- line-related styles
+	public var lineStyle:LineStyle;
+}
