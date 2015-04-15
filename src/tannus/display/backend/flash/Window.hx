4e7748cd2eb2069bca4435711f5b5ebae1b42b61
Ryan Davis <epikit@gmail.com>
2015-04-15 10:23:08 -0500

    Flash implementation of the Window class now has a "canvas" field, for drawing on, and no longer uses the EXIT_FRAME Event for its "frameEvent", but a Timer instead

--- src/tannus/display/backend/flash/Window.hx
+++ src/tannus/display/backend/flash/Window.hx
@@ -7,8 +7,11 @@ import tannus.io.Signal;
 
 import flash.Lib;
 import flash.display.Sprite;
+import flash.display.Shape;
 import flash.display.Graphics;
 import flash.external.ExternalInterface;
+
+import haxe.Timer;
 
 @:allow(tannus.display.backend.flash.EventBinder)
 class Window extends Sprite implements TWindow {
@@ -25,6 +28,7 @@ class Window extends Sprite implements TWindow {
 		cur.addChild( this );
 		stage.scaleMode = flash.display.StageScaleMode.NO_SCALE;
 
+		__prepareCanvas();
 		__init();
 	}
 
@@ -39,24 +43,48 @@ class Window extends Sprite implements TWindow {
 		var ebinder = new EventBinder(this);
 		ebinder.bind();
 		
-		stage.addEventListener(flash.events.Event.EXIT_FRAME, function(event):Void {
+		//stage.addEventListener(flash.events.Event.ENTER_FRAME, function(event):Void {
+			//__perFrameInternal();
+
+			//frameEvent.broadcast( null );
+		//});
+		var t:Timer = new Timer( 30 );
+		t.run = function() {
 			__perFrameInternal();
 
-			frameEvent.broadcast( null );
-		});
+			frameEvent.broadcast( nc_graphics );
+		};
+
+		__perFrameInternal();
 	}
 
 	/**
 	  * Nitty Gritty internals that need to be run per-frame
 	  */
 	private inline function __perFrameInternal():Void {
-		var g = graphics;
+		__prepareCanvas();
+
+		var g = canvas.graphics;
 		var a = nc_size;
 
 		/* === Draw Background === */
+		g.clear();
+
 		g.beginFill(nc_graphics.backgroundColor);
 		g.drawRect(0, 0, stage.stageWidth, stage.stageHeight);
 		g.endFill();
+	}
+
+	/**
+	  * Create and configure [canvas]
+	  */
+	public inline function __prepareCanvas():Void {
+		if (canvas != null) {
+			removeChild( canvas );
+		}
+
+		canvas = new Shape();
+		addChild( canvas );
 	}
 
 	/**
@@ -74,6 +102,11 @@ class Window extends Sprite implements TWindow {
 	public var frameEvent : Signal<Dynamic>;
 
 	public var nc_graphics : TGraphics;
+
+	/**
+	  * The Shape instance which we will use to draw on
+	  */
+	public var canvas:Shape;
 	
 /* === Computed Instance Fields === */
 
