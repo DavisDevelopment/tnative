e659c2f6d56ee0c353b4b24738690caf524932fa
Ryan Davis <epikit@gmail.com>
2015-04-15 10:13:32 -0500

    Added the LineCap enum

---
+++ src/tannus/graphics/LineCap.hx
@@ -0,0 +1,11 @@
+package tannus.graphics;
+
+/**
+  * Enum of all possible Line-Cap Types
+  */
+@:enum
+abstract LineCap (String) {
+	var Butt = 'butt';
+	var Round = 'round';
+	var Square = 'square';
+}
