a71c54b7aced66747f2edf4a8529e0b7a8fc5629
Ryan Davis <epikit@gmail.com>
2015-05-02 03:41:30 -0500

    Began the tannus.format package with the Writer base-class

---
+++ src/tannus/format/Writer.hx
@@ -0,0 +1,31 @@
+package tannus.format;
+
+import tannus.io.Byte;
+import tannus.io.ByteArray;
+import tannus.io.Ptr;
+import tannus.ds.Maybe;
+
+class Writer {
+	/* Constructor Function */
+	public function new():Void {
+		buffer = new ByteArray();
+	}
+
+/* === Instance Methods === */
+
+	/* Write some data onto [buffer] */
+	private inline function w(data : ByteArray) {
+		buffer.write( data );
+	}
+
+	/* Add a line-break to the buffer */
+	private inline function newline() w('\n');
+
+	/* Add a tab to the buffer */
+	private inline function tab(?n:Int=4) w([for (i in 0...n) ' '].join(''));
+
+
+/* === Instance Fields === */
+
+	private var buffer : ByteArray;
+}
