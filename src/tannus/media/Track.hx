1e6944c9e82a2e3828319ec6b0d19b7649799aad
Ryan Davis <epikit@gmail.com>
2015-05-02 04:11:22 -0500

    Added "duration" field to tannus.media.Track

--- src/tannus/media/Track.hx
+++ src/tannus/media/Track.hx
@@ -4,6 +4,7 @@ class Track {
 	/* Constructor Function */
 	public function new(nam:String, loc:String):Void {
 		index = -1;
+		duration = -1;
 		name = nam;
 		location = loc;
 	}
@@ -15,6 +16,9 @@ class Track {
 
 	/* The Track-Number of [this] Track */
 	public var index : Int;
+
+	/* The Duration of [this] Track, in Seconds */
+	public var duration : Int;
 
 	/* The Path or URL to [this] Track */
 	public var location : String;
