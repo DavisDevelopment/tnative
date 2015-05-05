0bf925c6e4ebb5b3db78bb7282573bc9ad3d6640
Ryan Davis <epikit@gmail.com>
2015-05-05 18:36:07 -0500

    Created initial (barebones) implementation of the Stage System (Rendering Loop)

---
+++ src/tannus/display/Entity.hx
@@ -0,0 +1,116 @@
+package tannus.display;
+
+import tannus.io.Ptr;
+import tannus.io.EventDispatcher;
+import tannus.ds.Object;
+import tannus.ds.Method;
+import tannus.ds.Maybe;
+
+import tannus.display.Stage;
+import tannus.graphics.GraphicsPath;
+
+import tannus.geom.*;
+
+class Entity extends EventDispatcher {
+	/* Constructor Function */
+	public function new():Void {
+		super();
+
+		addSignal( 'delete' );
+		addSignal( 'activate' );
+
+		position = new Point();
+		area = new Area();
+		root = null;
+		__remove = false;
+	}
+
+/* === Instance Methods === */
+
+	/**
+	  * Method called by Stage when [this] Entity is Attached to it
+	  */
+	private function attachTo(s : Stage):Void {
+		root = s;
+		dispatch('activate', s);
+	}
+
+	/**
+	  * Method Called Internally When [this] Entity is Removed from the Stage
+	  */
+	private function __delete():Void {
+		root.removeChild( this );
+		dispatch('delete', this);
+	}
+
+	/**
+	  * Method Called When [this] Entity is Removed from the Stage
+	  */
+	@:final
+	public function delete():Void {
+		__remove = true;
+	}
+
+	/**
+	  * Method called internally upon rendering
+	  */
+	@:final
+	private function __render(g : GraphicsPath):Void {
+		render( g );
+	}
+
+	/**
+	  * Method called each frame upon rendering
+	  */
+	public function render(g : GraphicsPath):Void {
+		null;
+	}
+
+	/**
+	  * Method called internally upon updating
+	  */
+	public function update():Void {
+		null;
+	}
+
+/* === Computed Instance Fields === */
+
+	/* The 'x' Coordinate of [this] Entity */
+	public var x(get, set):Float;
+	private inline function get_x() return position.x;
+	private inline function set_x(nx : Float) return (position.x = nx);
+
+	/* The 'y' Coordinate of [this] Entity */
+	public var y(get, set):Float;
+	private inline function get_y() return position.y;
+	private inline function set_y(nt : Float) return (position.y = nt);
+
+	/* The 'z' Coordinate of [this] Entity */
+	public var z(get, set):Float;
+	private inline function get_z() return position.z;
+	private inline function set_z(nz : Float) return (position.z = nz);
+
+	/* The 'width' of [this] Entity */
+	public var width(get, set):Float;
+	private inline function get_width() return area.width;
+	private inline function set_width(nw : Float) return (area.width = nw);
+	
+	/* The 'height' of [this] Entity */
+	public var height(get, set):Float;
+	private inline function get_height() return area.height;
+	private inline function set_height(nh : Float) return (area.height = nh);
+
+/* === Instance Fields === */
+
+	/* The Coordinates of [this] Entity */
+	public var position : Point;
+
+	/* The Area [this] Entity Covers */
+	public var area : Area;
+
+	/* The Stage [this] Entity is attached to */
+	private var root : Null<Stage>;
+
+	/* Whether [this] Entity is flagged for deletion */
+	private var __remove : Bool;
+}
---
+++ src/tannus/display/Stage.hx
@@ -0,0 +1,123 @@
+package tannus.display;
+
+import tannus.display.Window;
+import tannus.display.TGraphics;
+import tannus.display.Entity;
+import tannus.graphics.GraphicsPath;
+
+import tannus.geom.*;
+import tannus.io.Ptr;
+
+@:access(tannus.display.Entity)
+class Stage {
+	/* Constructor Function */
+	public function new(w : Window):Void {
+		window = w;
+		childNodes = new Array();
+		active = false;
+
+		__init();
+	}
+
+/* === Instance Methods === */
+
+	/**
+	  * Perform any/all initialization of [this] Stage
+	  */
+	private function __init():Void {
+		window.frameEvent.on(function(_x : Dynamic) {
+			if (active) {
+				frame();
+			}
+		});
+	}
+
+	/**
+	  * Method which will fire every frame
+	  */
+	private function frame():Void {
+		update();
+		render();
+	}
+
+	/**
+	  * Render all children of [this] Stage
+	  */
+	public function render():Void {
+		var mg = window.graphics.createPath();
+		var g:GraphicsPath;
+
+		for (ent in childNodes) {
+			g = mg.open();
+
+			ent.__render( g );
+
+			g.close();
+		}
+
+		mg.draw();
+	}
+
+	/**
+	  * 'update' [this] Stage
+	  */
+	public function update():Void {
+		for (ent in childNodes) {
+			if (ent.__remove) {
+				ent.__delete();
+			}
+		}
+
+		haxe.ds.ArraySort.sort(childNodes, function(a, b) {
+			return Std.int(a.z - b.z);
+		});
+
+		for (ent in childNodes) {
+			ent.update();
+		}
+	}
+
+	/**
+	  * Mark [this] Stage as 'active'
+	  */
+	public function activate():Void {
+		active = true;
+	}
+
+	/**
+	  * Mark [this] Stage as 'inactive'
+	  */
+	public function deactivate():Void {
+		active = false;
+	}
+
+	/**
+	  * Add an Entity onto [this] Stage
+	  */
+	public function addChild(child : Entity):Void {
+		childNodes.push( child );
+	}
+
+	/**
+	  * Remove an Entity from [this] Stage
+	  */
+	public function removeChild(child : Entity):Void {
+		childNodes.remove( child );
+	}
+
+/* === Instance Fields === */
+
+	/* The Window [this] Stage is attached to */
+	public var window : Window;
+
+	/* An Array of Entities attached to [this] Stage */
+	public var childNodes : Array<Entity>;
+
+	/* Whether [this] Stage is Currently Active */
+	private var active : Bool;
+
+/* === Class Fields === */
+
+	/* An Array of All Stage instances */
+	private static var instances : Array<Stage> = {new Array();};
+}
