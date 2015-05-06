082a8d7db85f8eff04a9c84db685742f2bcf2079
Ryan Davis <epikit@gmail.com>
2015-05-05 20:13:45 -0500

    Created tannus.ds.EitherType type, which can unify with either of two types, and allows safe/reliable checking which type was provided

---
+++ src/tannus/ds/EitherType.hx
@@ -0,0 +1,60 @@
+package tannus.ds;
+
+/**
+  * Abstract type allowing unification between two unrelated types, implicit casting to/from either type, AND a type-safe way to check which type was provided
+  */
+abstract EitherType<L, R> (Either<L, R>) {
+	/* Constructor Function */
+	public inline function new(e : Either<L, R>):Void {
+		this = e;
+	}
+
+	/**
+	  * Which type was provided, as a field
+	  */
+	public var type(get, never):Either<L, R>;
+	private inline function get_type() return this;
+
+	/**
+	  * Cast implicitly to the Left type
+	  */
+	@:to
+	public function toLeft():L {
+		switch (type) {
+			case Left( lv ):
+				return lv;
+
+			case Right( rv ):
+				throw 'EitherTypeError: $rv was not the expected value!';
+		}
+	}
+
+	/**
+	  * Cast implicitly to the Right type
+	  */
+	@:to
+	public function toRight():R {
+		switch (type) {
+			case Right( rv ):
+				return rv;
+
+			case Left( lv ):
+				throw 'EitherTypeError: $lv was not the expected value!';
+		}
+	}
+
+	@:from
+	public static function fromL<L, R>(v : L):EitherType<L, R> {
+		return new EitherType(Either.Left(v));
+	}
+
+	@:from
+	public static inline function fromR<L, R>(v : R):EitherType<L, R> {
+		return new EitherType(Either.Right(v));
+	}
+}
+
+enum Either<L, R> {
+	Left(value : L);
+	Right(value : R);
+}
