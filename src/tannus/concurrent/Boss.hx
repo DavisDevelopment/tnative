2c37f9c7a7048a5ff1953c2d7b6b4f4dda1cf01d
Ryan Davis <epikit@gmail.com>
2015-05-07 01:25:23 -0500

    Created tannus.concurrent.Boss, an abstract wrapper around the various implementations of Worker

---
+++ src/tannus/concurrent/Boss.hx
@@ -0,0 +1,31 @@
+package tannus.concurrent;
+
+import tannus.concurrent.IBoss;
+import haxe.macro.Context;
+
+using haxe.macro.ExprTools;
+@:forward
+abstract Boss<I, O> (IBoss<I, O>) from IBoss<I, O> to IBoss<I, O> {
+	/* Constructor Function */
+	public inline function new(b : IBoss<I, O>):Void {
+		this = b;
+	}
+
+	/**
+	  * The only global interface to obtain a Boss instance
+	  */
+	public static macro function hire( worker ) {
+		var fl = Context.getDefines();
+		trace(fl.keys());
+
+		/* JavaScript (Not NodeJS) Target */
+		if (fl.exists('js')) {
+			return macro new tannus.concurrent.Boss(tannus.concurrent.js.Workers.create($worker, '../dist/html/scripts/'));
+		}
+
+		/* Current Target Unsupported */
+		else {
+			return Context.error('WorkerError: Current Target Unsupported', Context.currentPos());
+		}
+	}
+}
