a7d615715255e878e6e210b9d51a57f91cee0136
Ryan Davis <epikit@gmail.com>
2015-05-16 17:35:56 -0500

    tannus.ds.TwoTuple class will now unify with python.Tuple2

--- src/tannus/ds/TwoTuple.hx
+++ src/tannus/ds/TwoTuple.hx
@@ -26,13 +26,35 @@ abstract TwoTuple<A, B> (T2<A, B>) {
 	private inline function get_two():B return this.b;
 	private inline function set_two(v:B):B return (this.b = v);
 
-	/**
-	  * Cast to a human-readable String
-	  */
+/* === Implicit Casting === */
+
+	/* Cast to a human-readable String */
 	@:to
 	public inline function toString():String {
 		return ('(' + Std.string(one) + ', ' + Std.string(two) + ')');
 	}
+
+	/* Cast to Array<Dynamic> */
+	@:to
+	public inline function toDynamicArray():Array<Dynamic> {
+		var ar:Array<Dynamic> = [one, two];
+		return ar;
+	}
+
+#if python
+	
+	/* Cast to python.Tuple2 */
+	@:to
+	public inline function toPythonTuple():python.Tuple.Tuple2<A, B> {
+		return python.Tuple.Tuple2.make(one, two);
+	}
+
+	/* Cast from python.Tuple2 */
+	@:from
+	public static inline function fromPythonTuple<A, B>(pt : python.Tuple.Tuple2<A, B>):TwoTuple<A, B> {
+		return new TwoTuple(pt._1, pt._2);
+	}
+#end
 }
 
 private typedef T2<A, B> = {
