0a0baf90bccc0ac9f63bac7eb63e66763d8e6cf2
Ryan Davis <epikit@gmail.com>
2015-05-17 03:25:51 -0500

    Removed unnecessary file from tannus/src/ directory

--- src/app.js
+++
@@ -1,7 +0,0 @@
-(function (console) { "use strict";
-var App = function() { };
-App.main = function() {
-	console.log("Hello, World!");
-};
-App.main();
-})(typeof console != "undefined" ? console : {log:function(){}});
