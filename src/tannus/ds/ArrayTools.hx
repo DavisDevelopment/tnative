e5f49afb63699e1d97d893ac768d23b97cf43ac9
Ryan Davis <epikit@gmail.com>
2015-05-17 01:22:59 -0500

    Added "without" method to tannus.ds.ArrayTools class

--- src/tannus/ds/ArrayTools.hx
+++ src/tannus/ds/ArrayTools.hx
@@ -2,12 +2,29 @@ package tannus.ds;
 
 import tannus.io.Ptr;
 
+using Lambda;
 class ArrayTools {
+	/**
+	  * Obtain an Array of Pointers from an Array of values
+	  */
 	public static function pointerArray<T>(a : Array<T>):Array<Ptr<T>> {
 		var res:Array<Ptr<T>> = new Array();
 		for (i in 0...a.length) {
 			res.push(Ptr.create(a[i]));
 		}
 		return res;
+	}
+
+	/**
+	  * Obtain a copy of [list] with all instances of [blacklist] removed
+	  */
+	public static function without<T>(list:Array<T>, blacklist:Array<T>):Array<T> {
+		var c = list.copy();
+		for (v in blacklist) {
+			while (true)
+				if (!c.remove(v))
+					break;
+		}
+		return c;
 	}
 }
