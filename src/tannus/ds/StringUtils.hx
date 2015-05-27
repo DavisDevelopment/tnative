e93db956f49caaa4902616df2165d1d8e30e2ec2
Ryan Davis <epikit@gmail.com>
2015-05-27 16:05:30 -0500

    Created new tannus.ds.StringUtils class

---
+++ src/tannus/ds/StringUtils.hx
@@ -0,0 +1,30 @@
+package tannus.ds;
+
+import tannus.ds.EitherType;
+import tannus.io.RegEx;
+
+using StringTools;
+
+/**
+  * Class with additional tools for manipulating Strings
+  */
+class StringUtils {
+	/**
+	  * Strip out all pieces of [str] which match the pattern [pat]
+	  */
+	public static function strip(str:String, pat:EitherType<String, EReg>):String {
+		switch (pat.type) {
+			case Left( repl ):
+				return str.replace(repl, '');
+
+			case Right( patt ):
+				var res:String = str;
+				var reg:RegEx = patt;
+				var bits = reg.matches(res);
+				for (bit in bits) {
+					res = res.replace(bit[0], '');
+				}
+				return res;
+		}
+	}
+}
