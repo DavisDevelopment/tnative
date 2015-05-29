9eef05b6c778f3c16024b3fde8c31563238ba993
Ryan Davis <epikit@gmail.com>
2015-05-29 10:19:52 -0500

    Created new tannus.format.pls.Reader class, for ready Playlist data from Playlist files

---
+++ src/tannus/format/pls/Reader.hx
@@ -0,0 +1,46 @@
+package tannus.format.pls;
+
+import tannus.io.ByteArray;
+import tannus.media.Playlist;
+import tannus.media.Track;
+
+using StringTools;
+class Reader {
+	/* Constructor Function */
+	public function new():Void {
+		reset();
+	}
+
+/* === Instance Methods === */
+
+	/**
+	  * Restore [this] instance to it's default state
+	  */
+	private inline function reset():Void {
+		playlist = new Playlist();
+	}
+
+	/**
+	  * Read the Playlist from a ByteArray
+	  */
+	public function read(data : ByteArray):Array<Track> {
+		var content:String = data.toString();
+		var lines:Array<String> = content.split('\n');
+		lines.shift();
+		var num:Int = Std.parseInt(lines.shift().split('=')[1]);
+
+		for (i in 1...num) {
+			var title:String = lines.shift().split('=')[1].substr( 1 );
+			var url:String = lines.shift().split('=')[1];
+
+			playlist.addTrack(title, url);
+		}
+
+		return playlist.tracks;
+	}
+
+/* === Instance Fields === */
+	
+	//- The Playlist instance which will hold all tracks read by [this] Reader
+	public var playlist : Playlist;
+}
