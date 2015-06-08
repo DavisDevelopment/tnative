d462ff6d58ee18e0079e71ad08cec9e48ddd4365
Ryan Davis <epikit@gmail.com>
2015-06-07 22:11:19 -0500

    Created the tannus.messaging.SocketMessage class

---
+++ src/tannus/messaging/SocketMessage.hx
@@ -0,0 +1,28 @@
+package tannus.messaging;
+
+import tannus.messaging.MessageType;
+import tannus.messaging.Socket;
+import tannus.ds.Object;
+import tannus.ds.Memory;
+
+class SocketMessage {
+	/* Constructor Function */
+	public function new(typ:MessageType, chann:String, dat:Object):Void {
+		channel = chann;
+		type = typ;
+		data = dat;
+		peer = null;
+		id = Memory.uniqueIdString('msg-');
+		sender = '';
+	}
+
+/* === Instance Fields === */
+
+	public var id:String;
+	public var sender:String;
+	public var channel:String;
+	public var type:MessageType;
+	public var data:Object;
+	public var peer:Null<String>;
+	public var reply:Null<Dynamic->Void>;
+}
