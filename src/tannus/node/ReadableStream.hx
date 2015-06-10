d8edb91e986874fd2dec6479e634f87353bc7cd4
Ryan Davis <epikit@gmail.com>
2015-06-10 00:51:47 -0500

    Created tannus.node.ReadableStream extern

---
+++ src/tannus/node/ReadableStream.hx
@@ -0,0 +1,24 @@
+package tannus.node;
+
+import tannus.node.Buffer;
+import tannus.node.EventEmitter;
+import tannus.node.WritableStream;
+
+@:jsRequire('stream', 'Readable')
+extern class ReadableStream extends EventEmitter {
+	function read(?size : Int):Null<Buffer>;
+
+	function setEncoding(enc : String):Void;
+
+	function resume():ReadableStream;
+
+	function pause():ReadableStream;
+
+	function isPaused():Bool;
+
+	function pipe(dest:WritableStream, opts:{end:Bool}):Void;
+
+	function unpipe(?dest : WritableStream):Void;
+
+	function unshift(chunk : Buffer):Void;
+}
