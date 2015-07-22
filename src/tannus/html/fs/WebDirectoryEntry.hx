95a1a33e79b181969f441498cba26b38e3af1f8c
Ryan Davis <epikit@gmail.com>
2015-07-22 15:39:17 -0500

    Created the tannus.html.fs.WebDirectoryEntry type

---
+++ src/tannus/html/fs/WebDirectoryEntry.hx
@@ -0,0 +1,89 @@
+package tannus.html.fs;
+
+import tannus.html.fs.WebFileSystem;
+import tannus.html.fs.WebFSEntry;
+import tannus.html.fs.WebFileEntry;
+import tannus.html.fs.WebFileError in Err;
+import tannus.html.fs.WebFileError.ErrorCode in Codes;
+import tannus.sys.Path;
+import tannus.ds.Object;
+import tannus.ds.Promise;
+import tannus.ds.promises.*;
+
+@:forward
+abstract WebDirectoryEntry (DirectoryEntry) from DirectoryEntry {
+	/* Constructor Function */
+	public inline function new(dir : DirectoryEntry):Void {
+		this = dir;
+	}
+
+/* === Instance Methods === */
+
+	/**
+	  * Check for the existence of a File in [this] Directory
+	  */
+	public inline function exists(path : Path):BoolPromise {
+		return Promise.create({
+			this.getFile(
+				path, null,
+				(function(entry) return true),
+				(function(error) {
+					switch(error.code) {
+						case NotFound:
+							return false;
+						default:
+							throw error;
+					}
+				})
+			);
+		}).bool();
+	}
+
+	/**
+	  * Create a new File
+	  */
+	public inline function createFile(path : Path):Promise<WebFileEntry> {
+		return new Promise(this.getFile.bind(path, {'create': true}));
+	}
+
+	/**
+	  * Get a reference to a File
+	  */
+	public inline function getFile(path:Path):Promise<WebFileEntry> {
+		return new Promise(this.getFile.bind(path, {}, _, _));
+	}
+
+	/**
+	  * Create a new Directory
+	  */
+	public inline function createDirectory(path : Path):Promise<WebDirectoryEntry> {
+		return new Promise(this.getDirectory.bind(path, {'create': true}));
+	}
+
+	/**
+	  * Obtain a List of Entries within [this] Directory
+	  */
+	public inline function readEntries():ArrayPromise<WebFSEntry> {
+		return new ArrayPromise(this.createReader().readEntries.bind(_, _));
+	}
+}
+
+typedef DirectoryEntry = {
+	> WebFSEntry,
+
+	/* Obtain a Reader for [this] Directory */
+	function createReader():DirectoryReader;
+
+	/* Get a File Reference */
+	function getFile(path:Path, options:Object, success:WebFileEntry->Void, failure:Err->Void):Void;
+
+	/* Get a Directory Reference */
+	function getDirectory(path:Path, options:Object, success:WebDirectoryEntry->Void, failure:Dynamic->Void):Void;
+
+	/* Delete [this] Directory */
+	function removeRecursively(success:Void->Void, failure:Dynamic->Void):Void;
+}
+
+typedef DirectoryReader = {
+	function readEntries(success:Array<WebFSEntry>->Void, ?failure:Err->Void):Void;
+};
