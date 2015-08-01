c4184821854dba02af6cfbbe26bd1aa1fde5d507
Ryan Davis <epikit@gmail.com>
2015-08-01 05:43:05 -0500

    Created the tannus.css.vals.Unit enum

---
+++ src/tannus/css/vals/Unit.hx
@@ -0,0 +1,42 @@
+package tannus.css.vals;
+
+@:enum
+abstract Unit (String) from String to String {
+/* === Constructs === */
+
+	var Em = 'em';
+	var Ex = 'ex';
+	var Ch = 'ch';
+	var Rem = 'rem';
+	var Vpw = 'vw';
+	var Vph = 'vh';
+	var Perc = '%';
+	var Cm = 'cm';
+	var Mm = 'mm';
+	var In = 'in';
+	var Px = 'px';
+	var Pt = 'pt';
+	var Pc = 'pc';
+
+/* === Static Methods === */
+
+	/**
+	  * Array of all Constructs
+	  */
+	public static var all(get, never):Array<String>;
+	private static inline function get_all():Array<String> {
+		return ([
+			/* Relative Units */
+			'em', 'ex', 'ch', 'rem', 'vw', 'vh', '%',
+			/* Absolute Units */
+			'cm', 'mm', 'in', 'px', 'pt', 'pc'
+		]);
+	}
+
+	/**
+	  * Test whether a given String is a valid Unit
+	  */
+	public static inline function isValidUnit(s : String):Bool {
+		return (Lambda.has(all, s));
+	}
+}
