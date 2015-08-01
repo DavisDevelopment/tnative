cfcc613ea5dc10b74e0892950e9d2bbd64d90091
Ryan Davis <epikit@gmail.com>
2015-08-01 05:41:25 -0500

    Created the tannus.css.Value enum

---
+++ src/tannus/css/Value.hx
@@ -0,0 +1,34 @@
+package tannus.css;
+
+import tannus.graphics.Color;
+import tannus.css.vals.Unit;
+
+/**
+  * Enum of all possible types of Property-values
+  */
+enum Value {
+/* === Used Values === */
+
+	/* Standard Identifier */
+	VIdent(id : String);
+
+	/* String */
+	VString(str : String);
+
+	/* Number */
+	VNumber(num:Float, ?unit:Unit);
+
+	/* Colors */
+	VColor(col : Color);
+
+	/* Variable Reference */
+	VRef(name : String);
+
+	/* Function Call */
+	VCall(func:String, args:Array<Value>);
+
+/* === Atomic Values (Not Used Directly) === */
+
+	/* Tuple */
+	VTuple(vals : Array<Value>);
+}
