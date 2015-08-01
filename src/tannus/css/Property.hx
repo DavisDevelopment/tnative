c4fa09eb408a280f457a918533ebf82df5390367
Ryan Davis <epikit@gmail.com>
2015-08-01 05:46:10 -0500

    Added readonly "values" field to the tannus.css.Property class

--- src/tannus/css/Property.hx
+++ src/tannus/css/Property.hx
@@ -1,10 +1,23 @@
 package tannus.css;
 
+import tannus.css.Value;
+import tannus.css.vals.Lexer.parseString;
+
 class Property {
 	/* Constructor Function */
 	public function new(key:String, val:String):Void {
 		name = key;
 		value = val;
+	}
+
+/* === Computed Instance Fields === */
+
+	/**
+	  * The value of [this] Property, as an Array of Value's
+	  */
+	public var values(get, never):Array<Value>;
+	private function get_values() {
+		return parseString(value);
 	}
 
 /* === Instance Fields === */
