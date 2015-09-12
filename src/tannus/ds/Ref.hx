d3c8fa835ed035e8c6d3db1c9af2dc31ca20837d
Ryan Davis <epikit@gmail.com>
2015-09-12 15:26:46 -0500

    Created tannus.ds.Ref type; a Getter that caches the value

---
+++ src/tannus/ds/Ref.hx
@@ -0,0 +1,59 @@
+package tannus.ds;
+
+import tannus.io.Getter;
+
+@:forward
+abstract Ref<T> (CRef<T>) from CRef<T> {
+	/* Constructor Function */
+	public inline function new(r : Getter<T>):Void {
+		this = new CRef(r);
+	}
+
+/* === Instance Methods === */
+
+	/* Cast to underlying type */
+	@:to
+	public inline function get():T return this.get();
+
+	/* Cast to String */
+	@:to
+	public inline function toString():String return Std.string(get());
+
+/* === Static Methods === */
+
+	/**
+	  * Create a Ref by macro
+	  */
+	public static macro function create<T>(v : haxe.macro.Expr.ExprOf<T>):haxe.macro.Expr.ExprOf<Ref<T>> {
+		return macro new tannus.ds.Ref(tannus.io.Getter.create($v));
+	}
+}
+
+@:generic
+class CRef<T> {
+	/* Constructor Function */
+	public function new(g : Getter<T>):Void {
+		getter = g;
+		_value = null;
+	}
+
+/* === Instance Methods === */
+
+	/**
+	  * Get the value of [this]
+	  */
+	public function get():T {
+		if (_value == null) {
+			trace('first get');
+			return (_value = getter.get());
+		}
+		else {
+			return _value;
+		}
+	}
+
+/* === Instance Fields === */
+
+	private var getter : Getter<T>;
+	private var _value : Null<T>;
+}
