2e302932ee3251aaa606f266502832cdd4575cdf
Ryan Davis <epikit@gmail.com>
2015-09-28 23:27:56 -0500

    Added ParametricStack type to the ActionStack module

--- src/tannus/ds/ActionStack.hx
+++ src/tannus/ds/ActionStack.hx
@@ -8,6 +8,8 @@ import haxe.macro.Context;
 import haxe.macro.Expr;
 import haxe.macro.ExprTools;
 
+using haxe.macro.ExprTools;
+
 @:forward
 abstract ActionStack (Array<Void -> Void>) {
 	/* Constructor Function */
@@ -31,11 +33,7 @@ abstract ActionStack (Array<Void -> Void>) {
 	  */
 	public inline function call():Void {
 		for (action in this) {
-			if (Reflect.isFunction(action)) {
-				action();
-			} else {
-				trace('Non-Function action found!');
-			}
+			action();
 		}
 	}
 
@@ -44,5 +42,45 @@ abstract ActionStack (Array<Void -> Void>) {
 	  */
 	public inline function clone():ActionStack {
 		return cast this.copy();
+	}
+}
+
+@:forward
+abstract ParametricStack<T> (Array<T -> Void>) {
+	/* Constructor Function */
+	public inline function new():Void {
+		this = new Array();
+	}
+
+/* === Instance Methods === */
+
+	/**
+	  * Add an Action onto [this] Stack
+	  */
+	public macro function append<T>(self:ExprOf<ParametricStack<T>>, action:Expr) {
+		function mapper(e : Expr) {
+			switch (e.expr) {
+				case ExprDef.EConst(CIdent('self')):
+					return {
+						'expr': EConst(CIdent('__context')),
+						'pos': Context.currentPos()
+					};
+
+				default:
+					return e.map(mapper);
+			}
+		}
+		action = action.map(mapper);
+		return macro $self.push(function(__context) {
+			$action;
+		});
+	}
+
+	/**
+	  * Call [this] Stack
+	  */
+	public inline function call(context : T):Void {
+		for (a in this)
+			a( context );
 	}
 }
