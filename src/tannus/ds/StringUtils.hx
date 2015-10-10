71f2fd6c6fde91eb74ff26622c049b383aec73bf
Ryan Davis <epikit@gmail.com>
2015-10-09 19:00:22 -0500

    Added 'before' and 'after' methods to the tannus.ds.StringUtils mixin

--- src/tannus/ds/StringUtils.hx
+++ src/tannus/ds/StringUtils.hx
@@ -57,4 +57,23 @@ class StringUtils {
 	public static function slice(str:String, pos:Int, ?len:Int):String {
 		return (len != null ? str.substr(pos, len) : str.substring(pos));
 	}
+
+	/**
+	  * Get all text of [s] that occurs BEFORE [del], or all of [s] if [del] is absent
+	  */
+	public static function before(s:String, del:String):String {
+		if (has(s, del)) {
+			return s.substring(0, s.indexOf(del));
+		}
+		else return s;
+	}
+
+	/**
+	  * Get all text of [s] that occurs AFTER [del], or all of [s] if [del] is absent
+	  */
+	public static function after(s:String, del:String):String {
+		if (has(s, del))
+			return s.substring(s.indexOf(del)+1);
+		else return s;
+	}
 }
