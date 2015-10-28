23bce77be05b5c0ddc09f40ec8e8d598108d8ebc
Ryan Davis <epikit@gmail.com>
2015-10-28 02:20:44 -0500

    tannus.geom.Angle type is no longer immutable

--- src/tannus/geom/Angle.hx
+++ src/tannus/geom/Angle.hx
@@ -2,6 +2,8 @@ package tannus.geom;
 
 import tannus.math.TMath;
 
+using tannus.math.TMath;
+
 /**
   * abstract class to represent a geometric angle(0 - 360) degrees
   */
@@ -11,18 +13,42 @@ abstract Angle (Float) from Float to Float {
 		this = degs;
 	}
 
+/* === Instance Methods === */
+
+	/**
+	  * Compliment of [this] Angle
+	  */
+	@:op( !A )
+	public inline function compliment():Angle {
+		return new Angle(360 - degrees);
+	}
+
+	/* post-increment */
+	@:op( A++ )
+	public inline function postincrement():Angle return new Angle(this++);
+	
+	/* pre-increment */
+	@:op( ++A )
+	public inline function preincrement():Angle return new Angle(++this);
+
 /* === Instance Fields === */
 
 	/* [this] Angle represented as degrees */
-	public var degrees(get, never):Float;
+	public var degrees(get, set):Float;
 	private inline function get_degrees():Float {
 		return this;
 	}
+	private inline function set_degrees(v : Float):Float {
+		return (this = v.clamp(0, 360));
+	}
 
 	/* [this] Angle represented as radians */
-	public var radians(get, never):Float;
+	public var radians(get, set):Float;
 	private inline function get_radians():Float {
-		return (this * (TMath.PI / 180));
+		return (this.toRadians());
+	}
+	private inline function set_radians(v : Float):Float {
+		return (degrees = v.toDegrees());
 	}
 
 	/**
