fdee1fe98d24ff2c90a66394a5fff04d6187e9bf
Ryan Davis <epikit@gmail.com>
2015-11-10 17:17:32 +0000

    Reimplemented tannus.ds.AsyncStack as a subclass of tannus.ds.Stack

--- tannus/ds/AsyncStack.hx
+++ tannus/ds/AsyncStack.hx
@@ -1,11 +1,51 @@
 package tannus.ds;
 
 import tannus.io.Signal;
+import tannus.io.VoidSignal;
 import haxe.macro.Expr;
+import tannus.ds.Async.Async;
 
 using haxe.macro.ExprTools;
 
-class AsyncStack {
+class AsyncStack extends Stack<Async> {
+	/* Constructor Function */
+	public function new():Void {
+		super();
+		completion = new VoidSignal();
+	}
+	
+/* === Instance Methods === */
+
+	/**
+	  * handle the next Async, and plan to do so again
+	  */
+	private function callNext():Void {
+		if ( !empty ) {
+			var action:Async = pop();
+			action( callNext );
+		}
+		else {
+			completion.call();
+		}
+	}
+	
+	/**
+	  * run the entire AsyncStack, and listen for it's completion
+	  */
+	public function run(done : Void->Void):Void {
+		if (empty) {
+			done();
+		}
+		else {
+			completion.once( done );
+			callNext();
+		}
+	}
+	
+	private var completion : VoidSignal;
+}
+
+class OldAsyncStack {
 	/* Constructor Function */
 	public function new():Void {
 		funcs = new Array();
