d625d393a39d11f172f532e32b43f6a6a0885835
Ryan Davis <epikit@gmail.com>
2015-11-10 02:42:18 -0600

    Added overloads to tannus.node.WritableStream

--- tannus/node/WritableStream.hx
+++ tannus/node/WritableStream.hx
@@ -8,11 +8,16 @@ import haxe.Constraints.Function;
 @:jsRequire('stream', 'Writable')
 extern class WritableStream extends EventEmitter {
 	/* write some data to [this] Stream */
+	@:overload(function(chunk:Buffer, ?cb:Function):Void {})
+	@:overload(function(chunk:Buffer, ?enc:String):Void {})
 	function write(chunk:Buffer, ?enc:String, ?cb:Function):Void;
 
 	function cork():Void;
 	function uncork():Void;
 	function setDefaultEncoding(enc : String):Void;
 
+	@:overload(function(chunk:Buffer, ?cb:Function):Void {})
+	@:overload(function(chunk:Buffer, ?enc:String):Void {})
+	@:overload(function(cb:Function):Void {})
 	function end(?chunk:Buffer, ?encoding:String, ?cb:Function):Void;
 }
