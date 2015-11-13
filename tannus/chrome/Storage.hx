1cdfe3d42f840a3c6f6ae893f83851b4ad39b09f
Ryan Davis <epikit@gmail.com>
2015-11-12 22:31:21 -0600

    refactored tannus.chrome.Storage.onChange to translate the StorageChange object into a Map of Deltas

--- tannus/chrome/Storage.hx
+++ tannus/chrome/Storage.hx
@@ -2,6 +2,7 @@ package tannus.chrome;
 
 import tannus.ds.Object;
 import tannus.ds.Maybe;
+import tannus.ds.Delta;
 import tannus.chrome.StorageArea;
 
 class Storage {
@@ -20,9 +21,22 @@ class Storage {
 	/**
 	  * Observe and handle changes to storage
 	  */
-	public static function onChange(listener:String->Object->Void):Void {
+	public static function onChange(listener : String -> StorageChange -> Void):Void {
 		lib.onChanged.addListener(function(changes:Object, area:String) {
-			listener(area, changes);
+			var change:StorageChange = new StorageChange();
+			for (key in changes.keys) {
+				var c:TStorageChange<Dynamic> = cast changes.get(key);
+				change.set(key, new Delta(c.newValue, c.oldValue));
+			}
+
+			listener(area, change);
 		});
 	}
 }
+
+typedef TStorageChange<T> = {
+	?oldValue : T,
+	?newValue : T
+};
+
+typedef StorageChange = Map<String, Delta<Dynamic>>;
