dfb9cdd59087a74cb233abdec7c04c97243b1649
Ryan Davis <epikit@gmail.com>
2015-11-19 01:41:29 -0600

    Deleted code for old implementation of tannus.ds.AsyncStack

--- tannus/ds/AsyncStack.hx
+++ tannus/ds/AsyncStack.hx
@@ -74,55 +74,5 @@ class AsyncStack extends Stack<Async> {
 	private var completion : VoidSignal;
 }
 
-class OldAsyncStack {
-	/* Constructor Function */
-	public function new():Void {
-		funcs = new Array();
-
-		complete = new Signal();
-	}
-
-/* === Instance Methods === */
-
-
-
-	/**
-	  * Calls the Func at index [i], if it exists
-	  */
-	private inline function call(i:Int, cb:Func):Void {
-		var f = funcs[i];
-		if (f != null)
-			funcs[i]( cb );
-		else
-			complete.call( null );
-	}
-
-	/**
-	  * Run the full Stack
-	  */
-	public function run(?done:Func):Void {
-		var i:Int = 0;
-
-		if (done != null) {
-			complete.on(function(x) done());
-		}
-		
-		function next():Void {
-			i++;
-			call(i, next);
-		}
-
-		call(i, next);
-	}
-
-/* === Instance Fields === */
-
-	/* Array of Callbacks */
-	private var funcs : Array<Callback>;
-
-	/* Function to run when [this] Stack is complete */
-	public var complete : Signal<Dynamic>;
-}
-
 private typedef Func = Void->Void;
 private typedef Callback = Func->Void;
