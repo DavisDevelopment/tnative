18cc466d46f7b7289d04a63275ce0b78ba721382
Ryan Davis <epikit@gmail.com>
2015-11-20 02:31:22 -0600

    Moved almost all methods of tannus.geom.Point from the abstract to the underlying class, and created new tannus.geom.LinkedPoint class

--- tannus/geom/Point.hx
+++ tannus/geom/Point.hx
@@ -6,6 +6,9 @@ import tannus.math.Percent;
 import tannus.geom.Angle;
 import tannus.ds.Maybe;
 import tannus.ds.EitherType;
+import tannus.io.Ptr;
+
+import haxe.macro.Expr;
 
 import Math.*;
 
@@ -15,7 +18,9 @@ import Math.*;
 #end
 
 using tannus.math.TMath;
+using tannus.macro.MacroTools;
 
+@:forward
 abstract Point (TPoint) {
 	/* Constructor Function */
 	public inline function new(?x:Float=0, ?y:Float=0, ?z:Float=0):Void {
@@ -69,69 +74,67 @@ abstract Point (TPoint) {
 	  * [this] Point's distance from (0, 0)
 	  */
 	public var d(get, never):Float;
-	private inline function get_d():Float return (distanceFrom(new Point()));
+	private inline function get_d():Float return (this.distanceFrom(new Point()));
 	
 /* === Instance Methods === */
 
-	/**
-	  * Calculate the distance between [this] and [other]
-	  */
-	public function distanceFrom(other : Point):Float {
-		return Math.sqrt(Math.pow(Math.abs(x-other.x), 2) + Math.pow(Math.abs(y-other.y), 2));
-	}
 
 	/**
 	  * Apply a Matrix to [this] Point
 	  */
 	public inline function transform(m : Matrix):Point {
-		return m.transformPoint(clone());
-	}
-
-	/**
-	  * Rotate [this] Point by the given Angle, around the given origin
-	  */
-	public function rotate(a:Angle, ?origin:Point):Point {
-		if (origin == null)
-			origin = new Point();
-		var s:Float = sin( a.radians );
-		var c:Float = cos( a.radians );
-		var nx:Float = (c * (x-origin.x) - s * (y-origin.y));
-		var ny:Float = (s * (x-origin.x) + c * (y-origin.y));
-		return new Point(nx, ny);
-	}
-
-	/**
-	  * Copy the state of [p] onto [this]
-	  */
-	public inline function copyFrom(p : Point):Void {
-		x = p.x;
-		y = p.y;
-		z = p.z;
+		return m.transformPoint(this.clone());
 	}
 
 	/**
 	  * Calculates the 'sum' of two Points
 	  */
 	@:op(A + B)
-	public function plus(other : Point):Point {
-		return new Point((x + other.x), (y + other.y), (z + other.z));
-	}
+	public inline function plusPoint(other : Point):Point return this.plusPoint( other );
+
+	/**
+	  * the sum of [this] and [n]
+	  */
+	@:op(A + B)
+	public inline function plusFloat(n : Float):Point return this.plusFloat( n );
+	@:op(A + B)
+	public inline function plusInt(n : Int):Point return this.plusFloat( n );
+
+	/**
+	  * Increment [this] Point by another
+	  */
+	@:op(A += B)
+	public inline function moveByPoint(p : Point):Point return this.moveByPoint( p );
+
+	/**
+	  * Increment [this] Point by a Float
+	  */
+	@:op(A += B)
+	public inline function moveByFloat(n : Float):Point return this.moveByFloat( n );
+	@:op(A += B)
+	public inline function moveByInt(n : Int):Point return this.moveByFloat( n );
 
 	/**
 	  * Calculate the difference between two points
 	  */
 	@:op(A - B)
-	public function minus(other : Point):Point {
-		return new Point((x - other.x), (y - other.y), (z - other.z));
-	}
+	public inline function minusPoint(p : Point):Point return this.minusPoint( p );
 
 	/**
 	  * Subtract [n] from [this] Point
 	  */
 	@:op(A - B)
-	public function minusFloat(n : Float):Point {
-		return new Point(x-n, y-n, z-n);
-	}
+	public inline function minusFloat(n : Float):Point return this.minusFloat( n );
+	@:op(A - B)
+	public inline function minusInt(n : Int):Point return this.minusFloat( n );
+
+	/**
+	  * Decrement [this] by [other]
+	  */
+	@:op(A -= B)
+	public inline function iminusPoint(p : Point):Point return this.iminusPoint(p);
+	@:op(A -= B)
+	public inline function iminusFloat(n : Float):Point return this.iminusFloat(n);
 
 	/**
 	  * Divide a Point
@@ -155,9 +158,7 @@ abstract Point (TPoint) {
 
 	/* negate */
 	@:op( -A )
-	public inline function negate():Point {
-		return this.multFloat( -1 );
-	}
+	public inline function negate():Point return this.negate();
 
 	/**
 	  * Compare two Points, based on their distance from (0, 0)
@@ -173,38 +174,10 @@ abstract Point (TPoint) {
 	}
 
 	@:op(A == B)
-	public inline function equals(other : Point):Bool {
-		return (x == other.x && y == other.y && z == other.z);
-	}
+	public inline function equals(p : Point):Bool return this.equals(p);
 
 	@:op(A != B)
-	public inline function nequals(other : Point):Bool {
-		return !equals(other);
-	}
-
-	/**
-	  * Calculate the Angle that exists between two Points
-	  */
-	public function angleTo(other : Point):Angle {
-		var angl:Float = TMath.angleBetween(x, y, other.x, other.y);
-		return Angle.fromDegrees( angl );
-	}
-
-	/**
-	  * Create and return a copy of [this] Point
-	  */
-	public inline function clone():Point {
-		return new Point(x, y, z);
-	}
-
-	/**
-	  * Round off [this] Point's coordinates to Ints
-	  */
-	public inline function clamp():Void {
-		this.x = ix;
-		this.y = iy;
-		this.z = iz;
-	}
+	public inline function nequals(p : Point):Bool return this.nequals(p);
 
 	/**
 	  * Vectorize [this] Point, based on a given Rectangle
@@ -227,29 +200,19 @@ abstract Point (TPoint) {
 	  * Casts [this] Point to a human-readable String
 	  */
 	@:to
-	public inline function toString():String {
-		return 'Point($x, $y, $z)';
-	}
+	public inline function toString():String return this.toString();
 
 	/* To Array<Float> */
 	@:to
-	public inline function toArray():Array<Float> {
-		return [x, y, z];
-	}
+	public inline function toArray():Array<Float> return this.toArray();
 
 	/* From Array<Float> */
 	@:from
-	public static function fromFloatArray(a : Array<Float>):Point {
-		var ma:Array<Maybe<Float>> = cast a;
-		return new Point(ma[0].or(0), ma[1].or(0), ma[2].or(0));
-	}
+	public static inline function fromFloatArray(a : Array<Float>):Point return TPoint.fromFloatArray(a);
 
 	/* From Array<Int> */
 	@:from
-	public static function fromIntArray(a : Array<Int>):Point {
-		var ma:Array<Maybe<Int>> = cast a;
-		return new Point(ma[0].or(0), ma[1].or(0), ma[2].or(0));
-	}
+	public static inline function fromIntArray(a : Array<Int>):Point return TPoint.fromFloatArray(cast a);
 
 
 	#if flash
@@ -341,6 +304,22 @@ abstract Point (TPoint) {
 	private static inline function perc(what:Float, of:Float):Percent {
 		return Percent.percent(what, of);
 	}
+
+	/**
+	  * Create a linked Point, explicitly
+	  */
+	public static inline function createLinked(x:Ptr<Float>, y:Ptr<Float>, ?z:Ptr<Float>):Point {
+		return cast new LinkedPoint(x, y, z);
+	}
+
+	/**
+	  * Create a linked Point, implicitly
+	  */
+	public static macro function linked(x:ExprOf<Float>, others:Array<ExprOf<Float>>):ExprOf<Point> {
+		var args:Array<ExprOf<Ptr<Float>>> = ([x].concat(others)).map(function(e) return e.pointer());
+		var result:ExprOf<Point> = (macro tannus.geom.Point.createLinked( $a{args} ));
+		return result;
+	}
 }
 
 /* Base Point Class */
@@ -353,6 +332,93 @@ class TPoint {
 	}
 
 /* === Instance Methods === */
+
+	/**
+	  * Get the Angle between [this] and [other]
+	  */
+	public function angleTo(other : Point):Angle {
+		return new Angle(TMath.angleBetween(x, y, other.x, other.y));
+	}
+
+	/**
+	  * Calculate the distance between [this] and [other]
+	  */
+	public function distanceFrom(other : Point):Float {
+		return Math.sqrt(Math.pow(Math.abs(x-other.x), 2) + Math.pow(Math.abs(y-other.y), 2));
+	}
+
+	/**
+	  * Copy the state of [p] onto [this]
+	  */
+	public function copyFrom(p : Point):Void {
+		x = p.x;
+		y = p.y;
+		z = p.z;
+	}
+
+	/**
+	  * Calculates the sum of [this] Point and another
+	  */
+	public function plusPoint(other : Point):Point {
+		return new Point((x + other.x), (y + other.y), (z + other.z));
+	}
+
+	/**
+	  * The sum of [this] and the given Float
+	  */
+	public function plusFloat(n : Float):Point {
+		return new Point((x + n), (y + n), (z + n));
+	}
+
+	/**
+	  * increment [this] by [other]
+	  */
+	public function moveByPoint(other : Point):Point {
+		x += other.x;
+		y += other.y;
+		z += other.z;
+		return cast this;
+	}
+
+	/**
+	  * increment [this] by [n]
+	  */
+	public function moveByFloat(n : Float):Point {
+		x += n;
+		y += n;
+		z += n;
+		return cast this;
+	}
+
+	/**
+	  * The difference between [this] and [other]
+	  */
+	public function minusPoint(other : Point):Point {
+		return new Point((x - other.x), (y - other.y), (z - other.z));
+	}
+
+	/**
+	  * subtract [n] from [this]
+	  */
+	public function minusFloat(n : Float):Point {
+		return new Point((x - n), (y - n), (z - n));
+	}
+
+	/**
+	  * decrement [this] by [other]
+	  */
+	public function iminusPoint(other : Point):Point {
+		moveByPoint( -other );
+		return cast this;
+	}
+
+	/**
+	  * decrement [this] by [n]
+	  */
+	public function iminusFloat(n : Float):Point {
+		moveByFloat( -n );
+		return cast this;
+	}
 
 	/**
 	  * Divide by another Point
@@ -382,6 +448,62 @@ class TPoint {
 		return (new Point(x*n, y*n, z*n));
 	}
 
+	/**
+	  * Negate [this] Point
+	  */
+	public function negate():Point {
+		return multFloat( -1 );
+	}
+
+	/**
+	  * Create and return a copy of [this] Point
+	  */
+	public function clone():Point {
+		return new Point(x, y, z);
+	}
+
+	/**
+	  * Clamp [this] Point
+	  */
+	public function clamp():Void {
+		x = i( x );
+		y = i( y );
+		z = i( z );
+	}
+
+	/**
+	  * A clamped version of [this] Point
+	  */
+	public function clamped():Point {
+		return new Point(i(x), i(y), i(z));
+	}
+
+	/**
+	  * Check for equality between [this] and [other]
+	  */
+	public function equals(other : Point):Bool {
+		return (x == other.x && y == other.y && z == other.z);
+	}
+
+	/* check for inequality between [this] and [other] */
+	public function nequals(other : Point):Bool {
+		return !equals( other );
+	}
+
+	/**
+	  * Convert to String
+	  */
+	public function toString():String {
+		return 'Point($x, $y, $z)';
+	}
+
+	/**
+	  * Convert to Array<Float>
+	  */
+	public function toArray():Array<Float> {
+		return [x, y, z];
+	}
+
 	private function get_x():Float return (_x);
 	private function get_y():Float return (_y);
 	private function get_z():Float return (_z);
@@ -399,4 +521,38 @@ class TPoint {
 	private var _x:Float;
 	private var _y:Float;
 	private var _z:Float;
+
+/* === Static Methods === */
+
+	/* from Array<Float> */
+	public static function fromFloatArray(a : Array<Float>):Point {
+		var ma:Array<Maybe<Float>> = cast a;
+		return new Point(ma[0].or(0), ma[1].or(0), ma[2].or(0));
+	}
+}
+
+/**
+  * Variant of TPoint whose x, y, z fields are pointers to values elsewhere
+  */
+class LinkedPoint extends TPoint {
+	/* Constructor Function */
+	public function new(x:Ptr<Float>, y:Ptr<Float>, ?z:Ptr<Float>):Void {
+		super(0, 0, 0);
+
+		rx = x;
+		ry = y;
+		rz = (z != null ? z : Ptr.create(_z));
+	}
+
+	override private function get_x():Float return (rx._);
+	override private function get_y():Float return (ry._);
+	override private function get_z():Float return (rz._);
+	
+	override private function set_x(v:Float):Float return (rx._ = v);
+	override private function set_y(v:Float):Float return (ry._ = v);
+	override private function set_z(v:Float):Float return (rz._ = v);
+
+	private var rx : Ptr<Float>;
+	private var ry : Ptr<Float>;
+	private var rz : Ptr<Float>;
 }
