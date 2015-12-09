28f7e7c88b1ef058f7951635cad7fdf5cca04795
Ryan Davis <epikit@gmail.com>
2015-12-08 18:44:57 -0600

    tannus.events.Event's internal Signals have been refactored to tannus.io.VoidSignals, and a new 'cancel' method was added

--- tannus/events/Event.hx
+++ tannus/events/Event.hx
@@ -2,6 +2,7 @@ package tannus.events;
 
 import tannus.io.Ptr;
 import tannus.io.Signal;
+import tannus.io.VoidSignal;
 import tannus.ds.Maybe;
 
 import tannus.geom.*;
@@ -14,26 +15,36 @@ class Event {
 		date = (Date.now().getTime());
 		_bubbles = bubbls;
 		_defaultPrevented = false;
+		_cancelled = false;
 
-		onDefaultPrevented = new Signal();
-		onPropogationStopped = new Signal();
+		onCancelled = new VoidSignal();
+		onDefaultPrevented = new VoidSignal();
+		onPropogationStopped = new VoidSignal();
 	}
 
 /* === Instance Methods === */
+
+	/**
+	  * Cancel [this] Event
+	  */
+	public function cancel():Void {
+		_cancelled = true;
+		onCancelled.fire();
+	}
 
 	/**
 	  * Prevent the default action of [this] Event
 	  */
 	public function preventDefault():Void {
 		_defaultPrevented = true;
-		onDefaultPrevented.broadcast( defaultPrevented );
+		onDefaultPrevented.fire();
 	}
 
 	/**
 	  * Stop the propogation of [this] Event
 	  */
 	public function stopPropogation():Void {
-		onPropogationStopped.broadcast( true );
+		onPropogationStopped.fire();
 	}
 
 /* === Computed Instance Fields === */
@@ -43,6 +54,10 @@ class Event {
 	  */
 	public var bubbles(get, never):Bool;
 	private inline function get_bubbles() return (_bubbles);
+
+	/* whether [this] Event has been cancelled */
+	public var cancelled(get, never):Bool;
+	private inline function get_cancelled():Bool return _cancelled;
 
 	/**
 	  * Whether [this] Event has had it's default action prevented
@@ -64,9 +79,15 @@ class Event {
 	//- Whether [this] Event has had it's default action prevented
 	private var _defaultPrevented:Bool;
 
+	// whether [this] Event has been cancelled
+	private var _cancelled : Bool;
+
 	//- Signal which fires when [this] Event's default action is prevented
-	private var onDefaultPrevented:Signal<Dynamic>;
+	private var onDefaultPrevented : VoidSignal;
 
 	//- Signal which fires when [this] Event's propogation is stopped
-	private var onPropogationStopped:Signal<Dynamic>;
+	private var onPropogationStopped : VoidSignal;
+
+	//- Signal which fires when [this] Event gets cancelled
+	private var onCancelled : VoidSignal;
 }
