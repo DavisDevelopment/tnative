4b11f2f470e918fb418bd059d39886cd104312fb
Ryan Davis <epikit@gmail.com>
2015-12-15 19:30:56 -0600

    Finally finished the tannus.html.fs.WebFileSystem type

--- tannus/html/fs/WebFileSystem.hx
+++ tannus/html/fs/WebFileSystem.hx
@@ -1,6 +1,24 @@
 package tannus.html.fs;
 
-typedef WebFileSystem = {
+import tannus.html.Win;
+import tannus.html.fs.WebDirectoryEntry in Dir;
+
+@:forward
+abstract WebFileSystem (WebFS) from WebFS to WebFS {
+	/* Constructor Function */
+	private inline function new(w : WebFS):Void {
+		this = w;
+	}
+
+	/**
+	  * request a FileSystem for use
+	  */
+	public static inline function request(size:Int, cb:WebFileSystem->Void):Void {
+		Win.current.requestFileSystem(size, cb);
+	}
+}
+
+typedef WebFS = {
 	var name : String;
-	var root : Dynamic;
+	var root : Dir;
 }
