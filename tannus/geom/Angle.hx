d5dd034da920e67557d6cb27492b95162a408af8
Ryan Davis <epikit@gmail.com>
2015-12-24 15:24:56 -0600

    reimplemented tannus.geom.Angle as a class

--- tannus/geom/Angle.hx
+++ tannus/geom/Angle.hx
@@ -1,74 +1,91 @@
 package tannus.geom;
 
 import tannus.math.TMath;
+import Math.*;
 
 using tannus.math.TMath;
 
 /**
   * abstract class to represent a geometric angle(0 - 360) degrees
   */
-abstract Angle (Float) from Float to Float {
+@:forward
+abstract Angle (CAngle) from CAngle to CAngle {
 	/* Constructor Function */
-	public inline function new(degs : Float):Void {
-		this = degs;
+	public inline function new(v : Float):Void {
+		this = new CAngle( v );
+	}
+
+/* === Methods === */
+
+	/* convert [this] to a String */
+	@:to
+	public inline function toString():String return this.toString();
+
+	/* convert [this] to a Float */
+	@:to
+	public inline function toFloat():Float return this.toFloat();
+
+	/**
+	  * cast from a Float
+	  */
+	@:from
+	public static inline function fromFloat(v : Float):Angle {
+		return new Angle( v );
+	}
+}
+
+class CAngle {
+	/* Constructor Function */
+	public function new(_v : Float):Void {
+		v = _v;
 	}
 
 /* === Instance Methods === */
 
 	/**
-	  * Compliment of [this] Angle
+	  * convert [this] to a String
 	  */
-	@:op( !A )
-	public inline function compliment():Angle {
-		return new Angle(360 - degrees);
+	public function toString():String {
+		return (v + '\u00B0');
 	}
 
-	/* post-increment */
-	@:op( A++ )
-	public inline function postincrement():Angle return new Angle(this++);
-	
-	/* pre-increment */
-	@:op( ++A )
-	public inline function preincrement():Angle return new Angle(++this);
+	/**
+	  * convert [this] to a Float
+	  */
+	public function toFloat():Float {
+		return v;
+	}
+
+/* === Computed Instance Fields === */
+
+	/* the degrees in [this] shit */
+	public var degrees(get, set):Float;
+	private inline function get_degrees():Float return v;
+	private inline function set_degrees(_v : Float):Float {
+		return (v = _v);
+	}
+
+	/* the radians of [this] shit */
+	public var radians(get, set):Float;
+	private inline function get_radians():Float return (v * Math.PI / 180);
+	private inline function set_radians(_v : Float):Float {
+		v = (_v * (Math.PI / 180));
+		return radians;
+	}
+
+	/* the x-shit of [this] Angle */
+	public var x(get, never):Float;
+	private inline function get_x():Float {
+		return cos( radians );
+	}
+
+	/* the y-shit of [this] Angle */
+	public var y(get, never):Float;
+	private inline function get_y():Float {
+		return sin( radians );
+	}
 
 /* === Instance Fields === */
 
-	/* [this] Angle represented as degrees */
-	public var degrees(get, set):Float;
-	private inline function get_degrees():Float {
-		return this;
-	}
-	private inline function set_degrees(v : Float):Float {
-		return (this = v.clamp(0, 360));
-	}
-
-	/* [this] Angle represented as radians */
-	public var radians(get, set):Float;
-	private inline function get_radians():Float {
-		return (this.toRadians());
-	}
-	private inline function set_radians(v : Float):Float {
-		return (degrees = v.toDegrees());
-	}
-
-	/**
-	 * Converts [this] Angle to a human-readble String
-	 */
-	public inline function toString():String {
-		return (degrees + '\u00B0');
-	}
-
-	/**
-	  * Creates a new Angle from degrees
-	  */
-	public static inline function fromDegrees(fl : Float):Angle {
-		return new Angle(fl);
-	}
-
-	/**
-	  * Creates a new Angle from radians
-	  */
-	public static inline function fromRadians(fl : Float):Angle {
-		return new Angle(fl * (TMath.PI / 180));
-	}
+	private var v : Float;
 }
