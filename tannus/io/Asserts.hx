99aec38cd1f6525122a999902ba6d9ab016c38e1
Ryan Davis <epikit@gmail.com>
2015-12-24 15:28:35 -0600

    added 'assertType' method to tannus.io.Asserts

--- tannus/io/Asserts.hx
+++ tannus/io/Asserts.hx
@@ -6,6 +6,7 @@ import haxe.macro.Context;
 using StringTools;
 using Lambda;
 using haxe.macro.ExprTools;
+using tannus.macro.MacroTools;
 
 class Asserts {
 	/**
@@ -18,6 +19,20 @@ class Asserts {
 			}
 			else {
 				throw $error;
+			}
+		};
+	}
+
+	/**
+	  * Verify that the given expression is of the given Type
+	  */
+	public static macro function assertType(value:Expr, _type:Expr) {
+		var isType:Expr = macro Std.is($value, $_type);
+		var etn:String = _type.toString();
+		var tn:Expr = macro tannus.internal.TypeTools.typename($value);
+		return macro {
+			if (!($isType)) {
+				throw ('TypeError: Expected ' + $v{etn} + ' but got ' + $tn + '!');
 			}
 		};
 	}
