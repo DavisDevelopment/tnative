945a314767b0f8d17c19ba0ef4355678f5e167b4
Ryan Davis <epikit@gmail.com>
2016-02-16 14:52:07 -0600

    added 'locking' to tannus.storage.js.WebFileStorage to prevent concurrent operations, which break the object

--- tannus/storage/js/WebFileStorage.hx
+++ tannus/storage/js/WebFileStorage.hx
@@ -4,6 +4,7 @@ import tannus.storage.Storage;
 import tannus.ds.Object;
 import tannus.ds.Delta;
 import tannus.io.Signal;
+import tannus.io.VoidSignal;
 import tannus.io.ByteArray;
 import tannus.html.fs.WebFileEntry;
 import tannus.html.fs.WebFile;
@@ -21,6 +22,7 @@ class WebFileStorage extends Storage {
 		fe = _f;
 		f = null;
 		writer = null;
+		_onunlocked = new VoidSignal();
 	}
 
 /* === Instance Methods === */
@@ -59,50 +61,79 @@ class WebFileStorage extends Storage {
 	  * fetch stored data
 	  */
 	override private function _fetch(cb:Data->Void):Void {
-		// get the File
-		getFile(function(file) {
-			if (file.size == 0) {
-				cb(new Data());
-			}
-			else {
-				file.read().then(function(data) {
-					if (data == null) {
-						data = new ByteArray();
-					}
-					var odata:Object = Json.parse(data.toString());
-					cb( odata );
-				});
-			}
-		});
+		if ( !_locked ) {
+			// get the File
+			lock();
+			getFile(function(file) {
+				if (file.size == 0) {
+					cb(new Data());
+				}
+				else {
+					file.read().then(function(data) {
+						if (data == null) {
+							data = new ByteArray();
+						}
+						var odata:Object = Json.parse(data.toString());
+						unlock();
+						cb( odata );
+					});
+				}
+			});
+		}
+		else {
+			throw new js.Error( 'Attempted to perform an operation on a Storage while locked!' );
+		}
 	}
 
 	/**
 	  * push local data
 	  */
 	override private function _push(mdata:Data, cb:Err->Void):Void {
-		var start = Date.now().getTime();
-		getWriter(function( writer ) {
-			trace('got writer object');
-			var o:Object = mdata.toObject();
-			var sdata:String = Json.stringify(o, null, '    ');
-			var data:ByteArray = ByteArray.ofString( sdata );
-			writer.write(data, function(err : Null<Dynamic>) {
-				if (err != null) {
-					throw err;
-				}
-				writer.truncate( writer.position );
-				f = null;
-				var end = Date.now().getTime();
-				var took = (end - start);
-				trace('metadata PUSH operation took ${took}ms to complete');
-				cb( err );
+		if ( !_locked ) {
+			var start = Date.now().getTime();
+			lock();
+			getWriter(function( writer ) {
+				trace('got writer object');
+				var o:Object = mdata.toObject();
+				var sdata:String = Json.stringify(o, null, '    ');
+				var data:ByteArray = ByteArray.ofString( sdata );
+				writer.write(data, function(err : Null<Dynamic>) {
+					if (err != null) {
+						unlock();
+						throw err;
+					}
+					writer.truncate( writer.position );
+					f = null;
+					var end = Date.now().getTime();
+					var took = (end - start);
+					trace('metadata PUSH operation took ${took}ms to complete');
+					unlock();
+					cb( err );
+				});
 			});
-		});
+		}
+		else {
+			throw new js.Error( 'Attempted to perform an operation on a Storage while locked!' );
+		}
 	}
+
+	private function lock():Void {
+		_locked = true;
+	}
+
+	private function unlock():Void {
+		_locked = false;
+		_onunlocked.fire();
+	}
+
+	public inline function isLocked():Bool return _locked;
 
 /* === Instance Fields === */
 
 	private var fe : WebFileEntry;
 	private var writer : Null<WebFileWriter>;
 	private var f : Null<WebFile>;
+
+	private var _locked : Bool = false;
+	private var _onunlocked : VoidSignal;
 }
