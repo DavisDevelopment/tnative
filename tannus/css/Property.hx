61b1b4663f18328cdb1f50093ef3a75e96fa0db4
Ryan Davis <epikit@gmail.com>
2016-02-15 22:05:02 -0600

    added 'setValues' method to tannus.css.Property

--- tannus/css/Property.hx
+++ tannus/css/Property.hx
@@ -3,11 +3,25 @@ package tannus.css;
 import tannus.css.Value;
 import tannus.css.vals.Lexer.parseString;
 
+using tannus.ds.ArrayTools;
+using tannus.css.vals.ValueTools;
+
 class Property {
 	/* Constructor Function */
 	public function new(key:String, val:String):Void {
 		name = key;
 		value = val;
+	}
+
+/* === Instance Methods === */
+
+	/**
+	  * Set the value of [this] Property by an Array<Value> object
+	  */
+	public function setValues(val : Array<Value>):String {
+		var hunks:Array<String> = val.macmap(_.toString());
+		
+		return (value = hunks.join(' '));
 	}
 
 /* === Computed Instance Fields === */
