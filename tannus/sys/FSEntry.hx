baf92310b2cf34216f7b9aebde98a8f98eca5d64
Ryan Davis <epikit@gmail.com>
2016-02-19 23:02:42 -0600

    changed the error thrown by tannus.sys.FSEntry when given a null Path

--- tannus/sys/FSEntry.hx
+++ tannus/sys/FSEntry.hx
@@ -95,13 +95,18 @@ abstract FSEntry (FSEntryType) {
 
 	/* From Path */
 	@:from
-	public static inline function fromPath(p : Path):FSEntry {
-		if (FileSystem.exists(p)) {
+	public static function fromPath(p : Path):FSEntry {
+		if (FileSystem.exists( p )) {
 			if (FileSystem.isDirectory( p ))
 				return new FSEntry(Folder( p ));
 			else
 				return new FSEntry(File( p ));
-		} else throw 'IOError: Cannot create FSEntry instance for Path which does not exist';
+		} 
+		else {
+			var err = 'IOError: Cannot create FSEntry instance for non-existent Path("$p")';
+			trace( err );
+			throw err;
+		}
 	}
 
 	/* From String */
