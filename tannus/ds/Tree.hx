40ddeeae217d9da8df2f66997f7480ec01037bfc
Ryan Davis <epikit@gmail.com>
2016-03-22 19:23:22 -0500

    Created tannus.ds.Tree class

---
+++ tannus/ds/Tree.hx
@@ -0,0 +1,19 @@
+package tannus.ds;
+
+class Tree <T : TreeNode> {
+	/* Constructor Function */
+	public function new():Void {
+		null;
+	}
+
+/* === Instance Methods === */
+
+	/* add a Node to [this] Tree */
+	public function append(node : T):Void {
+
+	}
+
+/* === Instance Fields === */
+
+	public var root : T;
+}
