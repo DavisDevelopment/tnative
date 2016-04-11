08005eb5d413d1d9d1d1556ed7d8238cb6abefa4
Ryan Davis <epikit@gmail.com>
2016-04-11 12:52:02 -0500

    renamed tannus.html.fs.WebFileInputStream to tannus.html.fs.WebFileInput

--- tannus/html/fs/WebFileInput.hx
+++ tannus/html/fs/WebFileInput.hx
@@ -2,8 +2,8 @@ package tannus.html.fs;
 
 import tannus.io.Byte;
 import tannus.io.ByteArray;
-import tannus.io.ReadableStream;
-import tannus.io.ReadableByteStream;
+import tannus.io.Input;
+import tannus.io.ByteArrayInput;
 import tannus.math.TMath.*;
 
 import tannus.html.fs.WebFile;
@@ -13,7 +13,7 @@ using Lambda;
 using tannus.ds.ArrayTools;
 using tannus.math.TMath;
 
-class WebFileInputStream extends ReadableByteStream {
+class WebFileInput extends ByteArrayInput {
 	/* Constructor Function */
 	public function new(file : WebFile):Void {
 		super();
@@ -23,7 +23,6 @@ class WebFileInputStream extends ReadableByteStream {
 
 		// five hundred kilobytes
 		chunkSize = Std.int((1024 * 1024) / 2);
-		chunkCount = Std.int(src.size / chunkSize);
 	}
 
 /* === Instance Methods === */
@@ -35,10 +34,11 @@ class WebFileInputStream extends ReadableByteStream {
 		reader.seek( offset );
 		
 		function gotChunk(data : ByteArray):Void {
-			chunkCount--;
 			offset += data.length;
 			provide( data );
-			if (chunkCount == 0) {
+
+			/* check whether the end of input has been reached */
+			if (position == length) {
 				endOfInput();
 			}
 		}
@@ -46,12 +46,27 @@ class WebFileInputStream extends ReadableByteStream {
 		reader.read(chunkSize, gotChunk, reject);
 	}
 
+	/**
+	  * move to the given position
+	  */
+	override private function __position(?i : Int):Int {
+		if (i != null) {
+			offset = i;
+		}
+		return offset;
+	}
+
+	/**
+	  * get the size of the file
+	  */
+	override private function __size():Int {
+		return src.size;
+	}
+
 /* === Instance Fields === */
 
-	public var offset : Int = 0;
-	public var length : Int = -1;
+	private var offset : Int = 0;
 
 	private var src : WebFile;
 	private var reader : WebFileReader;
-	private var chunkCount : Int;
 }
