804a44b777f8e758de54ecfaa082f206bc6ee5b4
Ryan Davis <epikit@gmail.com>
2016-04-11 12:42:21 -0500

    finished implementing tannus.chrome.Clipboard, and now it actually works

--- tannus/chrome/Clipboard.hx
+++ tannus/chrome/Clipboard.hx
@@ -5,6 +5,7 @@ import tannus.io.*;
 import tannus.ds.*;
 
 import js.html.InputElement;
+import js.html.TextAreaElement;
 
 using StringTools;
 using tannus.ds.StringUtils;
@@ -17,35 +18,70 @@ class Clipboard {
 	  * read and return clipboard-data
 	  */
 	public static function getData():String {
-		inp.focus();
-		inp.value = '';
+		var i = inp();
+		i.focus();
 		doc.execCommand( 'paste' );
-		return inp.value;
+		var result = i.value;
+		i.remove();
+		return result;
 	}
 
 	/**
 	  * write clipboard data
 	  */
 	public static function setData(s : String):Void {
-		inp.focus();
-		inp.value = s;
-		inp.select();
-		inp.selectionStart = 0;
-		inp.selectionEnd = s.length;
-		doc.execCommand( 'copy' );
-		inp.value = '';
+		var i = inp();
+		i.focus();
+		i.value = s;
+		doc.execCommand( 'selectAll' );
+		i.select();
+		doc.execCommand('copy', false, null);
+		i.remove();
+	}
+
+	private static function inp():ClipboardElement {
+		var i:ClipboardElement = cast doc.createElement('textarea');
+		doc.body.appendChild( i );
+		return i;
+	}
+
+	private static var win(get, never):Win;
+	private static inline function get_win():Win {
+		return Win.current;
 	}
 
 	private static var doc(get, never):js.html.HTMLDocument;
-	private static inline function get_doc() return Win.current.document;
+	private static inline function get_doc():js.html.HTMLDocument {
+		return win.document;
+	}
 
-	private static var inp(get, never):InputElement;
-	private static function get_inp():InputElement {
+	/*
+	private static var inp(get, never):ClipboardElement;
+	private static function get_inp():ClipboardElement {
 		if (_inp == null) {
-			_inp = doc.createInputElement();
+			_inp = cast doc.createElement( 'textarea' );
+			doc.body.appendChild( _inp );
 		}
 		return _inp;
 	}
+	*/
 
-	private static var _inp:Null<InputElement> = null;
+	/*
+	private static function __init__():Void {
+		_inp = null;
+		bg = null;
+
+		Runtime.getBackgroundPage(function( bgp ) {
+			if (Win.current != bgp) {
+				bg = bgp;
+				_inp = null;
+			}
+		});
+	}
+	*/
+
+	//private static var _inp:Null<ClipboardElement>;
+	//private static var bg : Null<Win>;
 }
+
+private typedef ClipboardElement = TextAreaElement;
