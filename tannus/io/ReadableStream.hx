490d2c060ab39f1415b5d5529e6fe255e2b8dac5
Ryan Davis <epikit@gmail.com>
2016-04-10 19:33:02 -0500

    reimplimented tannus.io.ReadableStream

--- tannus/io/ReadableStream.hx
+++ tannus/io/ReadableStream.hx
@@ -1,13 +1,14 @@
 package tannus.io;
 
-import tannus.io.Signal;
-
+/**
+  * base-class for providing providing data to a 'reader' in chunks, rather than all at once
+  */
+@:allow( tannus.io.StreamAccessor )
 class ReadableStream<T> {
 	/* Constructor Functoin */
 	public function new():Void {
-		dataEvent = new Signal();
-		errorEvent = new Signal();
-		_buffer = new Array();
+		__b = new Array();
+		__eoi = false;
 
 		opened = false;
 		closed = false;
@@ -19,6 +20,7 @@ class ReadableStream<T> {
 	/**
 	  * listen for data on [this] Stream
 	  */
+	/*
 	public function ondata(cb : T->Void):Void {
 		dataEvent.on( cb );
 	}
@@ -26,19 +28,52 @@ class ReadableStream<T> {
 	/**
 	  * listen for errors on [this] Stream
 	  */
+	/*
 	public function onerror(cb : String->Void):Void {
 		errorEvent.on( cb );
 	}
 
 	/**
-	  * request a single piece of data
+	  * read a 'chunk' from [this] Stream
 	  */
-	public function get(cb : T->Void):Void {
-		dataEvent.once(function(data : T) {
-			cb( data );
-			close();
-		});
-		open();
+	public function read(provide:T->Void, ?reject:Err->Void):Void {
+		if ( !opened ) {
+			throw 'Error: ReadableStream must be opened (by calling the "open" method) before data can be read from it';
+		}
+		else if ( closed ) {
+			throw 'Error: Cannot read from a closed Stream';
+		}
+
+		if (__b.length > 0) {
+			provide(__b.shift());
+		}
+		else {
+			if (reject == null) {
+				reject = (function(err) throw err);
+			}
+
+			__get(
+				function(chunk : Null<T>) {
+					if (chunk == null) {
+						var error = 'No data available on ReadableStream';
+						reject( error );
+					}
+					else {
+						provide( chunk );
+					}
+				},
+				function(error : Err) {
+					reject( error );
+				}
+			);
+		}
+	}
+
+	/**
+	  * must be defined by sub-classes
+	  */
+	private function __get(provide:Null<T>->Void, reject:Err->Void):Void {
+		provide( null );
 	}
 
 	/**
@@ -56,57 +91,52 @@ class ReadableStream<T> {
 	}
 
 	/**
-	  * Pause [this] Stream
-	  */
-	public function pause():Void {
-		paused = true;
-	}
-
-	/**
-	  * Resume [this] Stream
-	  */
-	public function resume():Void {
-		paused = false;
-		flush();
-	}
-
-	/**
 	  * Write data to the Buffer
 	  */
 	private function buffer(d : T):Void {
-		_buffer.push( d );
+		__b.push( d );
 	}
 
 	/**
-	  * Prepare data for reading
+	  * report that the end of input has been reached
 	  */
-	private function provide(d : T):Void {
-		dataEvent.call( d );
+	private function endOfInput():Void {
+		__eoi = true;
 	}
 
 	/**
 	  * Either buffer or provide some data
 	  */
+	/*
 	private inline function write(d : T):Void {
 		(paused ? buffer : provide)( d );
 	}
+	*/
 
 	/**
 	  * Flush the Buffer
 	  */
+	/*
 	private function flush():Void {
 		for (item in _buffer) {
 			dataEvent.call( item );
 		}
 		_buffer = new Array();
 	}
+	*/
+
+/* === Computed Instance Fields === */
+
+	public var eoi(get, never):Bool;
+	private inline function get_eoi():Bool return __eoi;
 
 /* === Instance Fields === */
 
-	private var dataEvent : Signal<T>;
-	private var errorEvent : Signal<String>;
-	private var _buffer : Array<T>;
+	private var __b : Array<T>;
 	private var opened : Bool;
 	private var closed : Bool;
 	private var paused : Bool;
+	private var __eoi : Bool;
 }
+
+typedef Err = Dynamic;
