2265717fadf2d5dadf209cbbde0024d1ea9a0d6d
Ryan Davis <epikit@gmail.com>
2016-05-08 05:29:40 -0500

    Wrote basic (incomplete) implementation of a simple cross-platform Error class

---
+++ tannus/utils/JavaScriptError.hx
@@ -0,0 +1,10 @@
+package tannus.utils;
+
+import js.Error;
+
+class JavaScriptError extends Error {
+	/* Constructor Function */
+	public function new(message : String):Void {
+		super( message );
+	}
+}
