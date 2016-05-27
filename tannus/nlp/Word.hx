3b9f6ca408783257bd71d5861b29d844ee92c91a
Ryan Davis <epikit@gmail.com>
2016-05-27 00:16:25 -0500

    Added 'levenshtein' method to tannus.nlp.Word

--- tannus/nlp/Word.hx
+++ tannus/nlp/Word.hx
@@ -1,5 +1,10 @@
 package tannus.nlp;
 
+import tannus.io.ByteArray;
+
+import Math.*;
+import tannus.math.TMath.*;
+
 using StringTools;
 using tannus.ds.StringUtils;
 
@@ -31,6 +36,54 @@ abstract Word (String) from String {
 	@:to
 	public inline function toString():String {
 		return this;
+	}
+
+	/**
+	  * convert to ByteArray
+	  */
+	@:to
+	public inline function bytes():ByteArray {
+		return ByteArray.ofString( this );
+	}
+
+	/**
+	  * Compare to [other] using levenshtein algorithm
+	  */
+	public function levenshtein(other : Word):Int {
+		if (this.empty()) return other.length;
+		if (other.length == 0) return this.length;
+
+		var l = bytes();
+		var r = other.bytes();
+
+		var matrix:Array<Array<Int>> = new Array();
+		var i:Int = 0;
+		while (i < r.length) {
+			matrix[i] = [i];
+			i++;
+		}
+
+		var j:Int = 0;
+		while (j < l.length) {
+			matrix[0][j] = j;
+			j++;
+		}
+
+		i = 1;
+		while (i < r.length) {
+			j = 1;
+			while (j < l.length) {
+				if (r[i - 1] == l[j - 1]) {
+					matrix[i][j] = matrix[i -1][j - 1];
+				}
+				else {
+					matrix[i][j] = cast min(matrix[i-1][j-1]+1, min(matrix[i][j-1]+1, matrix[i-1][j]+1));
+				}
+				j++;
+			}
+			i++;
+		}
+		return matrix[r.length - 1][l.length - 1];
 	}
 
 /* === Instance Fields === */
