9083f19a631c26e336b3b2f46b6ed17b966cf388
Ryan Davis <epikit@gmail.com>
2016-06-11 04:15:30 -0500

    Added property defining methods to tannus.html.JSTools

--- tannus/html/JSTools.hx
+++ tannus/html/JSTools.hx
@@ -1,6 +1,7 @@
 package tannus.html;
 
 import tannus.ds.Obj;
+import tannus.io.*;
 
 import haxe.macro.Expr;
 import haxe.macro.Context;
@@ -11,6 +12,7 @@ using StringTools;
 using tannus.ds.StringUtils;
 using Lambda;
 using tannus.ds.ArrayTools;
+using Reflect;
 
 class JSTools {
 	/**
@@ -18,6 +20,17 @@ class JSTools {
 	  */
 	public static inline function arrayify<T>(o : Dynamic):Array<T> {
 		return cast (untyped __js__('Array.prototype.slice.call')(o, 0));
+	}
+
+	public static inline function defineGetter(o:Dynamic, name:String, value:Getter<Dynamic>):Void {
+		o.getProperty('__defineGetter__').call(o, name, value);
+	}
+	public static inline function defineSetter(o:Dynamic, name:String, value:Setter<Dynamic>):Void {
+		o.getProperty('__defineSetter__').call(o, name, value);
+	}
+	public static inline function definePointer(o:Dynamic, name:String, value:Ptr<Dynamic>):Void {
+		defineGetter(o, name, value.getter);
+		defineSetter(o, name, value.setter);
 	}
 
 /* === Private Shit === */
