d64a0781e3d764763eee9b84fd9a308ecac3c0c4
Ryan Davis <epikit@gmail.com>
2016-06-14 22:07:36 -0500

    Reimplemented tannus.html.ElStyles

--- tannus/html/ElStyles.hx
+++ tannus/html/ElStyles.hx
@@ -1,70 +1,153 @@
 package tannus.html;
 
 import tannus.io.Ptr;
-import tannus.ds.Maybe;
-import tannus.ds.Object;
-import tannus.ds.TwoTuple;
+import tannus.ds.*;
+
+import tannus.css.Value;
+import tannus.css.vals.*;
 
 import tannus.html.Element;
 
-abstract ElStyles (Styler) {
+import Std.*;
+
+using Lambda;
+using tannus.ds.ArrayTools;
+using StringTools;
+using tannus.ds.StringUtils;
+using tannus.ds.MapTools;
+using tannus.css.vals.ValueTools;
+using Reflect;
+
+@:forward
+abstract ElStyles (CElStyles) from CElStyles to CElStyles {
 	/* Constructor Function */
-	public inline function new(af : Styler):Void {
-		this = af;
+	private inline function new(e : Element):Void {
+		this = new CElStyles( e );
 	}
 
-	/**
-	  * Retrieve the given css-property value
-	  */
+/* === Operator Methods === */
+
 	@:arrayAccess
-	public inline function get(key : String):String {
-		return this([key]);
-	}
-
-	/**
-	  * Reassigns a css-property value
-	  */
+	public inline function get(name : String):Maybe<String> return this.get( name );
 	@:arrayAccess
-	public inline function set(key:String, val:Dynamic):String {
-		this([key, Std.string(val)]);
-		return this([key]);
+	public inline function set(name:String, value:String):String return this.set(name, value);
+
+/* === Casting Methods === */
+
+	@:from
+	public static inline function fromElement(e : Element):ElStyles {
+		return e.style;
 	}
 
 	/**
-	  * Copy [keys] from [other] onto [this]
+	  * factory method for ElStyles
 	  */
-	public inline function copy(other:ElStyles, keys:Array<String>):Void {
-		for (k in keys) set(k, other[k]);
-	}
-
-	/**
-	  * Obtain a pointer to the value of a particular css-property
-	  */
-	public inline function reference(key : String):Ptr<String> {
-		var s:ElStyles = new ElStyles(this);
-
-		return Ptr.create(s[key]);
-	}
-
-	/**
-	  * Get an Object of css-properties from an Array of keys
-	  */
-	public function gets(keys : Array<String>):Object {
-		var o:Object = {};
-		for (k in keys)
-			o[k] = get(k);
-		return o;
-	}
-
-	/**
-	  * Write an Object onto [this] Style Set
-	  */
-	@:op(A += B)
-	public function writeObject(o : Object):Void {
-		for (p in o.pairs()) {
-			set(p.name, Std.string(p.value));
+	public static function create(elem : Element):ElStyles {
+		var key:String = '__tannus_styles';
+		var css:Null<ElStyles> = elem.getProperty( key );
+		if (css == null) {
+			elem.setProperty(key, css = new ElStyles( elem ));
 		}
+		return css;
 	}
 }
 
-private typedef Styler = Array<String>->String;
+class CElStyles {
+	/* Constructor Function */
+	public function new(e : Element):Void {
+		elem = e;
+	}
+
+/* === Instance Methods === */
+
+	/**
+	  * Get the value of a singular css-property
+	  */
+	public function get(name : String):Maybe<String> {
+		return elem.css( name );
+	}
+
+	/**
+	  * Set the value of a css-property
+	  */
+	public function set(name:String, value:String):String {
+		elem.css(name, value);
+		return get( name );
+	}
+
+	/**
+	  * Check whether the given css-property even exists
+	  */
+	public inline function exists(name : String):Bool {
+		return get( name ).toBoolean();
+	}
+
+	/**
+	  * Obtain a Pointer to the given css-property
+	  */
+	public inline function reference(name : String):Ptr<String> {
+		return new Ptr(cast get.bind( name ), set.bind(name, _));
+	}
+
+	/**
+	  * parse and return the values of the given css-property
+	  */
+	public function values(name : String):Array<Value> {
+		var s = get( name );
+		if (s != null) {
+			return Lexer.parseString( s );
+		}
+		else return new Array();
+	}
+
+	/**
+	  * grab the values of several properties
+	  */
+	public function pluck(names : Array<String>):Object {
+		var o:Object = new Object({});
+		for (name in names) {
+			o[name] = get( name );
+		}
+		return o;
+	}
+	public inline function gets(names : Array<String>):Object return pluck( names );
+
+	/**
+	  * apply a Map<String, Dynamic> of properties to [this]
+	  */
+	public function applyMap(map : Map<String, Dynamic>):Void {
+		for (name in map.keys()) {
+			set(name, string(map.get( name )));
+		}
+	}
+
+	/**
+	  * apply a Dict<String, Dynamic> of properties to [this]
+	  */
+	@:access( tannus.ds.dict.StringDict )
+	public inline function applyDict(map : Dict<String, Dynamic>):Void {
+		applyMap(cast(map, tannus.ds.dict.StringDict<Dynamic>).m);
+	}
+
+	/**
+	  * apply an Object of properties to [this]
+	  */
+	public function applyObject(o : Object):Void {
+		for (name in o.keys) {
+			set(name, string(o.get( name )));
+		}
+	}
+	public inline function writeObject(o : Object):Void applyObject( o );
+	public inline function write(o : Object):Void writeObject( o );
+
+	/**
+	  * Copy the given properties from [other] to [this]
+	  */
+	public function copy(keys:Array<String>, other:ElStyles):Void {
+		applyObject(other.pluck( keys ));
+	}
+
+/* === Instance Fields === */
+
+	private var elem : Element;
+}
