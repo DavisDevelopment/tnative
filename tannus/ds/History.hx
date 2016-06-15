9e22e52799fc85fe0105b4e0742fbb1dd6af2e9e
Ryan Davis <epikit@gmail.com>
2016-06-14 22:13:23 -0500

    Created new tannus.ds.History type

---
+++ tannus/ds/History.hx
@@ -0,0 +1,62 @@
+package tannus.ds;
+
+using Lambda;
+using tannus.ds.ArrayTools;
+
+class History<T> {
+	/* Constructor Function */
+	public function new():Void {
+		stack = new Array();
+	}
+
+/* === Instance Methods === */
+
+	/**
+	  * Add a new entry
+	  */
+	public function add(entry : T):Void {
+		if (!stack.empty() && compare(entry, stack.last())) {
+			return ;
+		}
+
+		index = stack.push( entry );
+	}
+
+	/**
+	  * Get the value of the [i]th entry from the top
+	  */
+	public function peek(i : Int = 0):Null<T> {
+		return stack[stack.length - (i + 1)];
+	}
+
+	/**
+	  * Remove the top entry from the stack
+	  */
+	public function pop():Null<T> {
+		return stack.pop();
+	}
+
+	/**
+	  * Get the index of the given value in the stack
+	  */
+	private function find(value : T):Int {
+		for (i in 0...stack.length) {
+			if (compare(value, stack[i])) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	/**
+	  * Compare two values in [this]
+	  */
+	private function compare(x:T, y:T):Bool {
+		return (x == y);
+	}
+
+/* === Instance Fields === */
+
+	private var index : Int;
+	private var stack : Array<T>;
+}
