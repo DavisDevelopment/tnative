0b54cdbbd913b693b3a9cb94e0e63d38ea2d87f0
Ryan Davis <epikit@gmail.com>
2016-09-20 18:59:35 -0500

    created new tannus.geom2.LinkedPoint class

---
+++ tannus/geom2/LinkedPoint.hx
@@ -0,0 +1,41 @@
+package tannus.geom2;
+
+import tannus.io.Ptr;
+import tannus.ds.DataView;
+import tannus.ds.data.BoundData;
+
+import Math.*;
+import tannus.math.TMath.*;
+
+import haxe.macro.Expr;
+import haxe.macro.Context;
+
+using Lambda;
+using tannus.ds.ArrayTools;
+using tannus.math.TMath;
+using haxe.macro.ExprTools;
+using tannus.macro.MacroTools;
+
+class LinkedPoint<T:Float> extends Point<T> {
+	/* Constructor Function */
+	public function new(x_ref:Ptr<T>, y_ref:Ptr<T>, z_ref:Ptr<T>):Void {
+		super(untyped 0, untyped 0, untyped 0);
+
+		d = new BoundData([x_ref, y_ref, z_ref]);
+	}
+
+/* === Static Methods === */
+
+	/**
+	  * macro-licious method to create a LinkedPoint
+	  */
+	public static macro function create<T:Float>(values : Array<ExprOf<T>>):ExprOf<LinkedPoint<T>> {
+		values = values.map(function( e ) {
+			return macro tannus.io.Ptr.create( $e );
+		});
+		var x:ExprOf<Ptr<T>> = values[0];
+		var y:ExprOf<Ptr<T>> = values[1];
+		var z:ExprOf<Ptr<T>> = values[2];
+		return macro new LinkedPoint($x, $y, $z);
+	}
+}
