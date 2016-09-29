d6bd7508bac387cec61d65182ae04ad81da6b0cf
Ryan Davis <epikit@gmail.com>
2016-09-29 16:09:51 -0500

    created new tannus.math.BasicRandomNumberGenerator type

---
+++ tannus/math/random/BasicRandomNumberGenerator.hx
@@ -0,0 +1,34 @@
+package tannus.math.random;
+
+import Math.*;
+import tannus.math.TMath.*;
+
+class BasicRandomNumberGenerator implements RandomNumberGenerator {
+	/* Constructor Function */
+	public function new(?seed : Int):Void {
+		this.seed = (seed != null ? seed : floor(random() * INT_MAX));
+		this.state = this.seed;
+	}
+
+/* === Instance Methods === */
+
+	/**
+	  * get the current state of [this] Rng
+	  */
+	public function getState():Int return state;
+
+	/**
+	  * get the next Integer in the progression of this algorithm
+	  */
+	public function nextInt():Int {
+		return (state = cast ((1103515245.0 * state + 12345) % INT_MAX));
+	}
+
+/* === Instance Fields === */
+
+	// the original seed
+	private var seed : Int;
+
+	// the current state
+	private var state : Int;
+}
