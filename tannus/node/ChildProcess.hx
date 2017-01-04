df08bc9b32d70093bf535e253833d9eb3ea1c232
DavisDevelopment <epikit@gmail.com>
2017-01-04 17:53:10 -0600

    fleshed out the tannus.node.ChildProcess extern

--- tannus/node/ChildProcess.hx
+++ tannus/node/ChildProcess.hx
@@ -1,6 +1,16 @@
 package tannus.node;
 
+import haxe.Constraints.Function;
+
 @:jsRequire('child_process')
-extern class ChildProcess {
+extern class ChildProcess extends EventEmitter {
+	function kill(?signal:String):Void;
+	function disconnect():Void;
+	function send(message:Dynamic, callback:Function):Void;
+
+	var connected:Bool;
+
+	static function exec(cmd:String, options:Dynamic, callback:Null<Dynamic>->Buffer->Buffer->Void):ChildProcess;
 	static function execSync(cmd:String, ?opts:Dynamic):Buffer;
+	static function form(modulePath:String, ?args:Array<String>, ?options:Dynamic):ChildProcess;
 }
