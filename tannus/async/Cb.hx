aee2db8dca8cce04a380ef2411ed841c38b772ab
DavisDevelopment <epikit@gmail.com>
2017-07-09 08:40:38 -0500

    reworked tannus.async.Cb as an abstract around Callback

--- tannus/async/Cb.hx
+++ tannus/async/Cb.hx
@@ -1,23 +1,17 @@
 package tannus.async;
 
+@:forward
 @:callable
-abstract Cb<T> (?Dynamic->?T->Void) from ?Dynamic->?T->Void {
-    public inline function new(f : ?Dynamic->?T->Void):Void {
+abstract Cb<T> (Callback<T, Dynamic>) from Callback<T, Dynamic> {
+    public inline function new(f : Callback<T,Dynamic>) {
         this = f;
     }
 
     @:to
-    public inline function raise():Dynamic->Void return untyped f.bind(_, null);
-    
+    public inline function raise():Dynamic->Void return this.raise();
     @:to
-    public inline function yield():T->Void return untyped f.bind(null, _);
-    
+    public inline function yield():T->Void return this.yield();
     @:to
-    public inline function toVoid():Void->Void return void();
-    public inline function void(?val : T):Void->Void {
-        return f.bind(null, val);
-    }
-
-    public var f(get, never):?Dynamic->?T->Void;
-    private inline function get_f():?Dynamic->?T->Void return this;
+    public inline function toVoid():Void->Void return this.toVoid();
 }
+
