f7da2e9c97dbd81d7385638db5d7cfac7fed5632
DavisDevelopment <epikit@gmail.com>
2017-09-12 00:44:47 -0500

    tannus.async.VoidAsync now casts implicitly from AND to its underlying type

--- tannus/async/VoidAsync.hx
+++ tannus/async/VoidAsync.hx
@@ -1,7 +1,7 @@
 package tannus.async;
 
 @:callable
-abstract VoidAsync (VoidCb->Void) from VoidCb->Void {
+abstract VoidAsync (VoidCb->Void) from VoidCb->Void to VoidCb->Void {
     public inline function new(f : VoidCb->Void)
         this = f;
 }
