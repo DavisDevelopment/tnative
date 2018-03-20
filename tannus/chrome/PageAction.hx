fc6f36378e71ed01f04e5a09cb66f9a2243d1568
DavisDevelopment <epikit@gmail.com>
2018-03-19 22:53:58 -0500

    created new PageAction class

---
+++ tannus/chrome/PageAction.hx
@@ -0,0 +1,10 @@
+package tannus.chrome;
+
+import tannus.chrome.Tab;
+
+import tannus.ds.Object;
+import tannus.ds.EitherType;
+
+class PageAction {
+    public static var lib:Dynamic = {untyped __js__('chrome.pageAction');};
+}
