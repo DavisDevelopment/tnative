8272842d775d789c355bcf111947306d3177d17d
DavisDevelopment <epikit@gmail.com>
2018-05-17 18:45:03 -0500

    added a bunch of sweet-ass methods to VoidCb

--- tannus/async/VoidCb.hx
+++ tannus/async/VoidCb.hx
@@ -6,12 +6,65 @@ using tannus.FunctionTools;
 
 @:callable
 abstract VoidCb (?Dynamic->Void) from ?Dynamic->Void {
+    /* Constructor Function */
     public inline function new(f : ?Dynamic->Void):Void {
         this = f;
     }
 
-    public inline function wrap(body:VoidCb->?Dynamic->Void):VoidCb {
-        return body.bind(this, _);
+    /**
+      wrap [this] callback function
+     **/
+    public function wrap(body:VoidCb->?Dynamic->Void):VoidCb {
+        function _wrapped(?error: Dynamic) {
+            body(this, error);
+        }
+        return new VoidCb(_wrapped);
+    }
+    private inline function _wrap(body:VoidCb->?Dynamic->Void):VoidCb return new VoidCb( this ).wrap( body );
+
+    /**
+      ensure that [this] callback will be called with an error after [time_ms] milliseconds
+     **/
+    public function timeout(time_ms:Int, ?err:Dynamic, ?id:String):VoidCb {
+        if (id == null) {
+            id = 'Callback';
+        }
+        else {
+            id = 'Callback("$id")';
+        }
+
+        if (err == null) {
+            err = '$id timed out';
+        }
+
+        var timer = new haxe.Timer(time_ms);
+        timer.run = (function() {
+            this( err );
+        });
+
+        return _wrap(function(_, ?error:Dynamic) {
+            timer.stop();
+            _( error );
+        });
+    }
+
+    /**
+      ensure that [this] callback is non-null, replacing it with [noop] if it is
+     **/
+    public inline function nn():VoidCb {
+        return new VoidCb(this != null ? this : VoidCb.noop);
+    }
+
+    /**
+      actually throw the [error] if one is raised
+     **/
+    public function toss():VoidCb {
+        return _wrap(function(_, ?error) {
+            if (error != null) {
+                throw error;
+            }
+            _( error );
+        });
     }
 
     /**
