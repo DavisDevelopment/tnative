373ac94be5908c0ad2d194fecf98489f7cae4520
DavisDevelopment <epikit@gmail.com>
2018-06-22 06:36:08 -0500

    added 'nn' method to tannus.async.Cb

--- tannus/async/Cb.hx
+++ tannus/async/Cb.hx
@@ -17,8 +17,15 @@ abstract Cb<T> (Callback<T, Dynamic>) from Callback<T, Dynamic> {
     #if python @:native('_void') #end
     public inline function toVoid():Void->Void return this.toVoid();
 
+    public inline function nn():Cb<T> {
+        return new Cb(this != null ? this : Cb.noop);
+    }
+
     public static function noop<T>(?error:Dynamic, ?value:T):Void {
         if (error != null) {
+            #if (js && debug)
+                js.Lib.global.console.error( error );
+            #end
             trace( error );
         }
         else if (value != null) {
