5079ab8c44ada3a1a437b2b9863aab1e5eb09c56
DavisDevelopment <epikit@gmail.com>
2018-06-27 16:03:33 -0500

    created new tannus.async.OptionTools mixin class

---
+++ tannus/async/OptionTools.hx
@@ -0,0 +1,36 @@
+package tannus.async;
+
+import tannus.ds.Delta;
+import tannus.io.Signal;
+import tannus.io.Signal2;
+
+import tannus.async.promises.*;
+import tannus.async.Promise;
+
+import haxe.ds.Option;
+import haxe.extern.EitherType as Either;
+import haxe.macro.Expr;
+import haxe.macro.Context;
+
+import Slambda.fn;
+import Reflect.deleteField;
+
+using Slambda;
+using tannus.ds.ArrayTools;
+using tannus.FunctionTools;
+
+using haxe.macro.ExprTools;
+using haxe.macro.TypeTools;
+using haxe.macro.ComplexTypeTools;
+using tannus.macro.MacroTools;
+
+class OptionTools {
+    public static inline function isNone<T>(o: Option<T>):Bool return o.match(None);
+    public static inline function isSome<T>(o: Option<T>):Bool return o.match(Some(_));
+    public static inline function getValue<T>(o: Option<T>):Null<T> {
+        return switch o {
+            case null, None: null;
+            case Some(v): v;
+        };
+    }
+}
