6e28ca338875d91148289de735e9c55148b07cbf
DavisDevelopment <epikit@gmail.com>
2018-07-24 03:53:37 -0500

    reimplemented tannus.ds.Ref

--- tannus/ds/Ref.hx
+++ tannus/ds/Ref.hx
@@ -1,65 +1,20 @@
 package tannus.ds;
 
-import tannus.io.Getter;
+abstract Ref<T> (Array<T>) from Array<T> {
+    public inline function new(value: T) {
+        this = [value];
+    }
 
-@:forward
-abstract Ref<T> (CRef<T>) from CRef<T> {
-	/* Constructor Function */
-	public inline function new(r : Getter<T>):Void {
-		this = new CRef(r);
-	}
+    @:to
+    public inline function get():T return this[0];
+    public inline function set(v: T) {
+        this[0] = v;
+    }
 
-/* === Instance Methods === */
+    public var value(get, set): T;
+    inline function get_value():T return get();
+    inline function set_value(v: T):T return (this[0] = v);
 
-	/* Cast to underlying type */
-	@:to
-	public inline function get():T return this.get();
-
-	/* Cast to String */
-	@:to
-	public inline function toString():String return Std.string(get());
-
-/* === Static Methods === */
-
-	/**
-	  * Create a Ref by macro
-	  */
-	public static macro function create<T>(v : haxe.macro.Expr.ExprOf<T>):haxe.macro.Expr.ExprOf<Ref<T>> {
-		return macro new tannus.ds.Ref(tannus.io.Getter.create($v));
-	}
-}
-
-@:generic
-class CRef<T> {
-	/* Constructor Function */
-	public function new(g : Getter<T>):Void {
-		getter = g;
-		_value = null;
-	}
-
-/* === Instance Methods === */
-
-	/**
-	  * Get the value of [this]
-	  */
-	public function get():T {
-		if (_value == null) {
-			return (_value = getter.get());
-		}
-		else {
-			return _value;
-		}
-	}
-
-	/**
-	  * reset [this] Ref
-	  */
-	public inline function reset():Void {
-		_value = null;
-	}
-
-/* === Instance Fields === */
-
-	private var getter : Getter<T>;
-	private var _value : Null<T>;
+    @:from
+    public static inline function const<T>(v: T):Ref<T> return new Ref(v);
 }
